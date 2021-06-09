0e296719b59845cf0680fe60cdc6aaa664eb8c74
Barbara Hui <barbara.hui@ucop.edu>
2021-06-09 16:37:32 -0700

    Create thumbnails for PDFs and save to /tmp in docker container

--- file-fetcher-docker/file_fetchers/nuxeo_file_fetcher.py
+++ file-fetcher-docker/file_fetchers/nuxeo_file_fetcher.py
@@ -3,9 +3,13 @@ import boto3
 import requests
 import json
 from botocore.exceptions import ClientError
+import subprocess
 
 NUXEO_BASIC_USER = os.environ['NUXEO_BASIC_USER']
 NUXEO_BASIC_AUTH = os.environ['NUXEO_BASIC_AUTH']
+magick_convert_location = os.environ.get('PATH_MAGICK_CONVERT', '/usr/bin/convert')
+ffmpeg_location = os.environ.get('PATH_FFMPEG', '/usr/bin/ffmpeg')
+ffprobe_location = os.environ.get('PATH_FFPROBE','/usr/bin/ffprobe')
 
 class NuxeoFileFetcher(object):
 
@@ -55,34 +59,80 @@ class NuxeoFileFetcher(object):
         print(f"{s3_key=}")
 
         # check to see if key already exists on s3
+        # TODO: allow for clean restash
         try:
             response = s3.head_object(
                 Bucket=s3_bucket,
                 Key=s3_key
             )
-            print(f"{s3_key} already exists on s3")
-
+            s3_key_exists = True
         except ClientError as e:
             if e.response['Error']['Code'] == "404":
+                s3_key_exists = False
                 print(f"stashing {s3_key}")
-                # https://amalgjose.com/2020/08/13/python-program-to-stream-data-from-a-url-and-write-it-to-s3/
-                response = requests_session.post(source_url,
-                    auth=(NUXEO_BASIC_USER, NUXEO_BASIC_AUTH),
-                    stream=True)
-                with response as part:
-                    part.raw.decode_content = True
+
+        if 1 == 1:
+        #if s3_key_exists is False:
+
+            response = requests_session.get(source_url,
+                auth=(NUXEO_BASIC_USER, NUXEO_BASIC_AUTH),
+                stream=True)
+            response.raise_for_status()
+
+
+            with response as part:
+
+                ''' stash content file on s3
+                    https://amalgjose.com/2020/08/13/python-program-to-stream-data-from-a-url-and-write-it-to-s3/
+                    https://docs.aws.amazon.com/AmazonS3/latest/userguide/mpu-upload-object.html
+
+                '''
+                part.raw.decode_content = True
+                if s3_key_exists is False:
                     conf = boto3.s3.transfer.TransferConfig(multipart_threshold=10000, max_concurrency=4)
                     s3.upload_fileobj(part.raw, s3_bucket, s3_key, Config=conf)
 
-                # update media.json with s3_uri
+                # stash thumbnail on s3
+                if mimetype == 'application/pdf':
+                    print(f"create thumbnail and stash on s3 at {s3_key=}")
+                    # download copy of contentfile to /tmp
+                    local_filepath = f"/tmp/{filename}"
 
-        if mimetype == 'application/pdf':
-            print(f"{mimtype=} - download file to /tmp so we can create PDF thumbnail")
-        elif mimetype.startswith('video'):
-             print(f"{mimetype=} - download file to /tmp so we can create a video thumbnail")
-        elif mimetype.startswith('image'):
-            print(f"{mimetype=} - download file to /tmp so we can create a jp2")
+                    with open(local_filepath, 'wb') as f:
+                        for block in part.iter_content(chunk_size=None):
+                            f.write(block)
 
+                    # create thumbnail
+                    self.create_pdf_thumbnail(local_filepath, calisphere_id)
+
+                    # updoad thumbnail to s3
+                    s3_key = f"nuxeo-thumbnails/{calisphere_id}::thumbnail.png"
+
+
+                    # delete local files
+
+                elif mimetype.startswith('video'):
+                    s3_key = f"nuxeo-thumbnails/{calisphere_id}::thumbnail.png"
+                    print(f"create thumbnail and stash on s3 at {s3_key=}")
+                     # TODO: create and stash thumbnail
+                elif mimetype.startswith('image'):
+                    s3_key = f"nuxeo-high-res-images/{calisphere_id}::{filename}.jp2"
+                    print(f"create jp2000 and stash on s3 at {s3_key=}")
+                    # TODO: create and stash jp2
+
+            # update media.json with s3_uri?
+
+        else:
+            print(f"{s3_key} already exists on s3")
+
+
+    def create_pdf_thumbnail(self, input_path, calisphere_id):
+        """ stash thumbnail image of PDF on s3 """
+        output_path = f"{input_path}-thumb.png"
+        input_string = f"{input_path}[0]"  # [0] to specify first page of PDF
+
+        result = subprocess.run([magick_convert_location, '-quiet', '-density', '300', input_string, '-quality', '90', output_path])
+        # TODO: deal with exceptions
 
     def stash_large_format_image(self):
         """ stash jp2 image for display in frontend """
