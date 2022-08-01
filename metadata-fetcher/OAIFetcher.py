12cb3c20a3374f97228a00159d87a8160c32067e
amywieliczka <amywieliczka@gmail.com>
2022-08-01 11:52:04 -0700

    Working tree updates to OAIFetcher

--- metadata-fetcher/OAIFetcher.py
+++ metadata-fetcher/OAIFetcher.py
@@ -1,6 +1,7 @@
 import json
 import xmltodict
 from Fetcher import Fetcher, FetchError
+from urllib.parse import parse_qs
 
 
 class OAIFetcher(Fetcher):
@@ -8,22 +9,61 @@ class OAIFetcher(Fetcher):
     #     "url": (
     #             "http://www.adc-exhibits.museum.ucsb.edu/"
     #             "oai-pmh-repository/request"),
-    #     "set": "38",
+    #     "extra_data": "metadataPrefix=oai_dpla&set=member_of_collection_ids_ssim:3vb00000zz-89112",
+    #     "resumption_token": ""
+    # }
+    # params['oai'] = {
+    #     "url": (
+    #             "http://www.adc-exhibits.museum.ucsb.edu/"
+    #             "oai-pmh-repository/request"),
+    #     "extra_data": "big-pine-citizen-newspaper",
+    #     "resumption_token": ""
+    # }
+    # params['oai'] = {
+    #     "url": (
+    #             "http://www.adc-exhibits.museum.ucsb.edu/"
+    #             "oai-pmh-repository/request"),
+    #     "resumption_token": ""
+    # }
+    # params['oai'] = {
+    #     "url": (
+    #             "http://www.adc-exhibits.museum.ucsb.edu/"
+    #             "oai-pmh-repository/request"),
+    #     "metadata_prefix": "oai_dpla",
+    #     "metadata_set": "member_of_collection_ids_ssim:3vb00000zz-89112",
     #     "resumption_token": ""
     # }
     def __init__(self, params):
         super(OAIFetcher, self).__init__(params)
         self.oai = params.get('oai')
 
+        # default metadata_prefix is oai_dc, default metadata_set is None
+        if 'metadata_prefix' not in self.oai:
+            self.oai['metadata_prefix'] = 'oai_dc'
+        if 'metadata_set' not in self.oai:
+            self.oai['metadata_set'] = None
+
+        # metadata_prefix must be set explicitly in extra_data
+        # metadata_set can be set implicitly or explicitly
+        if 'extra_data' in self.oai:
+            params = parse_qs(self.oai['extra_data'])
+            if 'metadataPrefix' in params:
+                self.oai['metadata_prefix'] = params['metadataPrefix'][0]
+            if 'set' in params:
+                self.oai['metadata_set'] = params['set'][0]
+            else:
+                self.oai['metadata_set'] = self.oai['extra_data']
+
     def build_fetch_request(self):
         url = self.oai.get('url')
-        oai_set = self.oai.get('set')
+        metadata_set = self.oai.get('metadata_set')
+        metadata_prefix = self.oai.get('metadata_prefix')
         resumption_token = self.oai.get('resumption_token')
 
         if not resumption_token:
             request = {"url": (
                     f'{url}?verb=ListRecords'
-                    f'&metadataPrefix=oai_dc&set={oai_set}'
+                    f'&metadataPrefix={metadata_prefix}&set={metadata_set}'
                 )}
         else:
             request = {"url": (
@@ -32,7 +72,7 @@ class OAIFetcher(Fetcher):
             )}
 
         print(
-            f"Fetching page "
+            f"{self.collection_id}: Fetching page "
             f"at {request.get('url')}")
         return request
 
@@ -45,7 +85,7 @@ class OAIFetcher(Fetcher):
 
         documents = []
         for record in records:
-            doc = record.get('metadata').get('oai_dc:dc')
+            doc = record.get('metadata')
             doc.update({
                 "calisphere-id": self.build_id(record)
             })
