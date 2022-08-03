0d7fecd3830ee9ddf4561163dc7185889105abb7
amywieliczka <amywieliczka@gmail.com>
2022-08-03 09:13:40 -0700

    Changed OAIFetcher.get_records to OAIFetcher.check_page

--- metadata-fetcher/test_data/oai_harvests.py
+++ metadata-fetcher/test_data/oai_harvests.py
@@ -1,86 +1,52 @@
-# OAI Harvests
-oai_27435_sugoroku = {
+oai_harvests = [
+  { # 27435: Sugoroku - metadata_prefix & metadata_set explicitly specified
     'collection_id': 27435,
-    'harvest_type': 'oai',
+    'harvest_type': "OAIFetcher",
     'write_page': 0,
     'oai': {
         'url': "https://digicoll.lib.berkeley.edu/oai2d",
-        'metadataPrefix': "marcxml",
-        'oai_set': "sugoroku"
+        'metadata_prefix': "marcxml",
+        'metadata_set': "sugoroku"
     }
-}
-oai_27836_ae_hanson_landscape_designs = {
-  "collection_id": 27836,
-  "harvest_type": "OAIFetcher",
-  "write_page": 0,
-  "oai": {
-    "url": (
-      "http://www.adc-exhibits.museum.ucsb.edu/"
-      "oai-pmh-repository/request"),
-    "set": 38
-  }
-}
-oai_large_26673_old_series_trademarks = {
-  "collection_id": 26673,
-  "harvest_type": "OAIFetcher",
-  "write_page": 0,
-  "oai": {
-    "url": "http://exhibits.sos.ca.gov/oai-pmh-repository/request",
-    "set": 1
-  }
-}
-
-# OAI Harvests
-oai_27435_sugoroku = {
-    "collection_id": 27435,
+  },
+  { # 27836: AE Hanson Landscape Designs - metadata_set explicitly specified
+    "collection_id": 27836,
     "harvest_type": "OAIFetcher",
     "write_page": 0,
     "oai": {
-        "url": "https://digicoll.lib.berkeley.edu/oai2d",
-        "metadata_prefix": "marcxml",
-        "metadata_set": "sugoroku"
+      "url": (
+        "http://www.adc-exhibits.museum.ucsb.edu/"
+        "oai-pmh-repository/request"),
+      "metadata_set": 38
     }
-}
-
-oai_26693_complex_extra_data = {
-  "collection_id": 26796,
-  "harvest_type": "OAIFetcher",
-  "write_page": 0, 
-  "oai": {
-    "url": "https://digital.lib.calpoly.edu/oai2/oai.php",
-    "extra_data": "metadataPrefix=oai_dc&set=rekl_panamerican-ms159"
+  },
+  # { # 26673: Old Series Trademarks - large collection, simple extra_data, API seems to be down
+  #   "collection_id": 26673,
+  #   "harvest_type": "OAIFetcher",
+  #   "write_page": 0,
+  #   "oai": {
+  #     "url": "http://exhibits.sos.ca.gov/oai-pmh-repository/request",
+  #     "extra_data": "1"
+  #   }
+  # },
+  { # 26693: Fritz-Metcalf - complex extra data
+    "collection_id": 26796,
+    "harvest_type": "OAIFetcher",
+    "write_page": 0, 
+    "oai": {
+      "url": "https://digital.lib.calpoly.edu/oai2/oai.php",
+      "extra_data": "metadataPrefix=oai_dc&set=rekl_panamerican-ms159"
+    }
+  },
+  { # 2206: Big Pine Citizen Newspaper - simple extra data
+    "collection_id": 2206,
+    "harvest_type": "OAIFetcher",
+    "write_page": 0,
+    "oai": {
+      "url": "http://oai.quartexcollections.com/lmudigitalcollections/oai2",
+      "extra_data": "big-pine-citizen-newspaper"
+    }
   }
-}
+]
 
-oai_27836_ae_hanson_landscape_designs = {
-  "collection_id": 27836,
-  "harvest_type": "OAIFetcher",
-  "write_page": 0,
-  "oai": {
-    "url": (
-      "http://www.adc-exhibits.museum.ucsb.edu/"
-      "oai-pmh-repository/request"),
-    "metadata_set": 38
-  }
-}
 
-# doesn't work?
-# oai_large_26673_old_series_trademarks = {
-#   "collection_id": 26673,
-#   "harvest_type": "OAIFetcher",
-#   "write_page": 0,
-#   "oai": {
-#     "url": "http://exhibits.sos.ca.gov/oai-pmh-repository/request",
-#     "extra_data": 1
-#   }
-# }
-
-oai_2206 = {
-  "collection_id": 2206,
-  "harvest_type": "OAIFetcher",
-  "write_page": 0,
-  "oai": {
-    "url": "http://oai.quartexcollections.com/lmudigitalcollections/oai2",
-    "extra_data": "big-pine-citizen-newspaper"
-  }
-}
--- metadata-fetcher/tests.py
+++ metadata-fetcher/tests.py
@@ -1,9 +1,10 @@
 from lambda_function import lambda_handler
 from test_data.nuxeo_harvests import *
 from test_data.oac_harvests import *
+from test_data.oai_harvests import *
 import json
 
-harvests = oac_harvests
+harvests = oai_harvests
 #  + nuxeo_complex_object_harvests + nuxeo_nested_complex_object_harvests
 for harvest in harvests:
     print(f"tests.py: {json.dumps(harvest)}")
