e966ab1d5c97e97c6a8a248e8dc7904836738978
amywieliczka <amywieliczka@gmail.com>
2022-10-12 08:54:57 -0700

    Removed OAI import statement

--- metadata-fetcher/lambda_function.py
+++ metadata-fetcher/lambda_function.py
@@ -7,7 +7,6 @@ import subprocess
 from Fetcher import Fetcher, FetchError
 from NuxeoFetcher import NuxeoFetcher
 from OACFetcher import OACFetcher
-from OAIFetcher import OAIFetcher
 
 DEBUG = os.environ.get('DEBUG', False)
 
