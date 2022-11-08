5aab59e2f984e248f619fa5fb8a80c62deb749cc
amywieliczka <amywieliczka@gmail.com>
2022-11-08 13:30:41 -0800

    Set 's3' as default data source and destination location

--- metadata-mapper/metadata_mapper/settings.py
+++ metadata-mapper/metadata_mapper/settings.py
@@ -1,8 +1,8 @@
 import os
 
 LOCAL_RUN = os.environ.get('MAPPER_LOCAL_RUN', False)
-DATA_SRC = os.environ.get('MAPPER_DATA_SOURCE', False)
-DATA_DEST = os.environ.get('MAPPER_DATA_DEST', False)
+DATA_SRC = os.environ.get('MAPPER_DATA_SRC', 's3')
+DATA_DEST = os.environ.get('MAPPER_DATA_DEST', 's3')
 
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')    # doesn't currently do anything
 S3_BUCKET = os.environ.get('S3_BUCKET', False)
--- metadata_fetcher/settings.py
+++ metadata_fetcher/settings.py
@@ -1,7 +1,7 @@
 import os
 
 LOCAL_RUN = os.environ.get('FETCHER_LOCAL_RUN', False)
-DATA_DEST = os.environ.get('FETCHER_DATA_DEST', False)
+DATA_DEST = os.environ.get('FETCHER_DATA_DEST', 's3')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')    # doesn't currently do anything
 S3_BUCKET = os.environ.get('S3_BUCKET', False)
 TOKEN = os.environ.get('NUXEO')
