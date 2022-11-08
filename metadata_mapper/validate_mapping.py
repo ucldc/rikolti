0c7825b82284abe98db1ab6f7721b7c1300686d3
amywieliczka <amywieliczka@gmail.com>
2022-11-08 13:56:01 -0800

    Validation report is still in development

---
+++ metadata_mapper/validate_mapping.py
@@ -0,0 +1,174 @@
+import sys
+import os
+import boto3
+import requests
+import json
+
+DEBUG = os.environ.get('DEBUG', False)
+SOLR_URL = os.environ.get('UCLDC_SOLR_URL', False)
+SOLR_API_KEY = os.environ.get('UCLDC_SOLR_API_KEY', False)
+
+
+def local_path(folder, collection_id):
+    parent_dir = os.sep.join(os.getcwd().split(os.sep)[:-1])
+    local_path = os.sep.join([
+        parent_dir,
+        'rikolti_bucket',
+        folder,
+        str(collection_id),
+    ])
+    return local_path
+
+
+def solr(**params):
+    solr_url = f'{SOLR_URL}/query/'
+    solr_auth = {'X-Authentication-Token': SOLR_API_KEY}
+    query = {}
+    for key, value in list(params.items()):
+        key = key.replace('_', '.')
+        query.update({key: value})
+    res = requests.post(solr_url, headers=solr_auth, data=query, verify=False)
+    res.raise_for_status()
+    results = json.loads(res.content.decode('utf-8'))
+    # facet_counts = results.get('facet_counts', {})
+    return results['response']['docs'][0]
+
+
+def validate_mapped_page(collection_id, page_filename):
+    if DEBUG:
+        page_path = os.sep.join([
+            local_path('mapped_metadata', collection_id),
+            str(page_filename)
+        ])
+        page = open(page_path, "r")
+        mapped_metadata = page.read()
+    else:
+        s3 = boto3.resource('s3')
+        bucket = 'rikolti'
+        key = f"mapped_metadata/{collection_id}/{page_filename}"
+        s3_obj_summary = s3.Object(bucket, key).get()
+        mapped_metadata = s3_obj_summary['Body'].read()
+
+    # repository_fields = [
+    #     'campus_data',
+    #     'campus_name',
+    #     'campus_url',
+    #     'collection_data',
+    #     'collection_name',
+    #     'collection_url',
+    #     'repository_data',
+    #     'repository_name',
+    #     'repository_url',
+    #     'sort_collection_data',
+    # ]
+
+    excluded_fields = [
+        'reference_image_md5',
+        'reference_image_dimensions',
+        'structmap_url',
+        'url_item',
+        'harvest_id_s'
+    ]
+
+    # search_fields = [
+    #     'facet_decade',
+    #     'sort_date_end',
+    #     'sort_date_start',
+    #     'sort_title',
+    # ]
+
+    # harvest_fields = [
+    #     '_version_',
+    #     'harvest_id_s',
+    #     'timestamp',
+    # ]
+
+    # enrichment_fields = (
+    #     repository_fields +
+    #     excluded_fields +
+    #     search_fields +
+    #     harvest_fields
+    # )
+
+    print(
+        "Rikolti Record Id | CouchDB Id | Field Name | Solr Value | "
+        "Rikolti Value"
+    )
+    mapped_metadata = json.loads(mapped_metadata)
+    for rikolti_record in mapped_metadata:
+        # TODO: really inefficient to query for each item individually;
+        # could we filter by collection and retrieve first 100 items in solr?
+        # would need to manage solr pagination against rikolti pagination,
+        # no guarantee the order will be the same...but maybe it's close
+        # enough to just take a greedy approach?
+        # Also, as updates are made to the mapper to accomodate future
+        # collections, we'd want to ensure that we aren't breaking any past
+        # collections that already work. Again, it's inefficient to keep
+        # hitting solr, but maybe this is an optimization for much later.
+
+        query = {"q": rikolti_record['calisphere-id']}
+        solr_record = solr(**query)
+
+        rikolti_fields = [field for field in rikolti_record.keys()]
+
+        for field, value in solr_record.items():
+            if field in excluded_fields:
+                continue
+
+            if field[-3:] == "_ss":
+                field = field[:-3]
+
+            if field not in rikolti_record:
+                print(
+                    f"{rikolti_record['calisphere-id']} | "
+                    f"{solr_record['harvest_id_s']} | "
+                    f"{field} | {value} | "
+                    f"NONE"
+                )
+                continue
+
+            if field in rikolti_record and rikolti_record.get(field) != value:
+                print(
+                    f"{rikolti_record['calisphere-id']} | "
+                    f"{solr_record['harvest_id_s']} | "
+                    f"{field} | {value} | "
+                    f"{rikolti_record[field]}"
+                )
+                rikolti_fields.pop(rikolti_fields.index(field))
+                continue
+
+        for field in rikolti_fields:
+            print(
+                f"{rikolti_record['calisphere-id']} | "
+                f"{solr_record['harvest_id_s']} | "
+                f"{field} | NONE | "
+                f"{rikolti_record[field]}"
+            )
+
+        if (rikolti_record['calisphere-id'] ==
+                'ccf27d23-738b-4b88-a1de-afeef5e9bda7'):
+            break
+
+
+def validate_mapped_collection(payload):
+    if DEBUG:
+        payload = json.loads(payload)
+
+    collection_id = payload.get('collection_id')
+
+    if DEBUG:
+        mapped_path = local_path('mapped_metadata', collection_id)
+        page_list = [f for f in os.listdir(mapped_path)
+                     if os.path.isfile(os.path.join(mapped_path, f))]
+
+    for page in page_list:
+        validate_mapped_page(collection_id, page)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Validate mapped metadata against SOLR")
+    parser.add_argument('payload', help='json payload')
+    args = parser.parse_args(sys.argv[1:])
+    validate_mapped_collection(args.payload)
