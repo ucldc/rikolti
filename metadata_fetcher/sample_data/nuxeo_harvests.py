29afacc8de31e1106a88399c93a257d0aaf3542e
amywieliczka <amywieliczka@gmail.com>
2022-12-11 22:24:14 -0500

    Update sample nuxeo collections to use 'harvest_data' and 'root_path' keys

--- metadata_fetcher/sample_data/nuxeo_harvests.py
+++ metadata_fetcher/sample_data/nuxeo_harvests.py
@@ -8,40 +8,40 @@ nuxeo_harvests = [
     "collection_id": 26695,
     "harvest_type": "nuxeo",
     "write_page": 0,
-    "nuxeo": {
-      "path": "/asset-library/UCI/SCA_UniversityArchives/Publications/Anthill/"
+    "harvest_data": {
+      "root_path": "/asset-library/UCI/SCA_UniversityArchives/Publications/Anthill/"
     }
   }, 
   { # 26098: Ramicova
       'collection_id': 26098,
       'harvest_type': 'nuxeo',
       'write_page': 0,
-      'nuxeo': {
-          'path': "/asset-library/UCM/Ramicova/"
+      "harvest_data": {
+          'root_path': "/asset-library/UCM/Ramicova/"
       }
   },
   { # 26746: Nordskog Papers
     "collection_id": 26746,
     "harvest_type": "nuxeo",
     "write_page": 0,
-    "nuxeo": {
-      "path": "/asset-library/UCR/Water Resources Collections & Archives/Andrae B. Nordskog papers"
+    "harvest_data": {
+      "root_path": "/asset-library/UCR/Water Resources Collections & Archives/Andrae B. Nordskog papers"
     }
   },
   { # 26697: Spectrum
     "collection_id": 26697,
     "harvest_type": "nuxeo",
     "write_page": 0,
-    "nuxeo": {
-      "path": "/asset-library/UCI/SCA_UniversityArchives/Publications/Spectrum/"
+    "harvest_data": {
+      "root_path": "/asset-library/UCI/SCA_UniversityArchives/Publications/Spectrum/"
     }
   },
   { # 27694: Halpern
     "collection_id": 27694,
     "harvest_type": "nuxeo",
     "write_page": 0,
-    "nuxeo": {
-      "path": "/asset-library/UCR/SCUA/Archival/MS075",
+    "harvest_data": {
+      "root_path": "/asset-library/UCR/SCUA/Archival/MS075",
       "fetch_children": True
     }
   },
@@ -49,8 +49,8 @@ nuxeo_harvests = [
     "collection_id": 27141,
     "harvest_type": "nuxeo",
     "write_page": 0,
-    "nuxeo": {
-      "path": "/asset-library/UCR/SCUA/Archival/UA042/published",
+    "harvest_data": {
+      "root_path": "/asset-library/UCR/SCUA/Archival/UA042/published",
       "fetch_children": True
     }
   }
@@ -61,32 +61,32 @@ nuxeo_complex_object_harvests = [
     'collection_id': 466,
     'harvest_type': 'nuxeo',
     'write_page': 0,
-    'nuxeo': {
-        'path': "/asset-library/UCSF/MSS 2000-31 AIDS Ephemera Collection/"
+    "harvest_data": {
+        "root_path": "/asset-library/UCSF/MSS 2000-31 AIDS Ephemera Collection/"
     }
   },
   { # 27012: UC Cooperative Extension
     "collection_id": 27012,
     "harvest_type": "nuxeo",
     "write_page": 0,
-    "nuxeo": {
-      "path": "/asset-library/UCM/UCCE/Merced/PUBLISH/"
+    "harvest_data": {
+      "root_path": "/asset-library/UCM/UCCE/Merced/PUBLISH/"
     }
   },
   { # 68: McLean
     "collection_id": 68,
     "harvest_type": "nuxeo",
     "write_page": 0,
-    "nuxeo": {
-      "path": "/asset-library/UCM/McLean/Publish/"
+    "harvest_data": {
+      "root_path": "/asset-library/UCM/McLean/Publish/"
     }
   },
   { # 76: Nightingale
     "collection_id": 76,
     "harvest_type": "nuxeo",
     "write_page": 0,
-    "nuxeo": {
-      "path": "/asset-library/UCM/NightingaleDiaries/",
+    "harvest_data": {
+      "root_path": "/asset-library/UCM/NightingaleDiaries/",
       "fetch_children": True
     }
   }
@@ -97,8 +97,8 @@ nuxeo_nested_complex_object_harvests = [
     "collection_id": 14256,
     "harvest_type": "nuxeo",
     "write_page": 0,
-    "nuxeo": {
-      "path": "/asset-library/UCM/Wilma_McDaniel/Publish/"
+    "harvest_data": {
+      "root_path": "/asset-library/UCM/Wilma_McDaniel/Publish/"
     }
   }
 ]
