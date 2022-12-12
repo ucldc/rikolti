01f19a80d902d4843a047e6b799bd829f5e66d5b
amywieliczka <amywieliczka@gmail.com>
2022-12-11 22:21:26 -0500

    Update oai sample harvests with harvest_data key

--- metadata_fetcher/sample_data/oai_harvests.py
+++ metadata_fetcher/sample_data/oai_harvests.py
@@ -3,8 +3,7 @@ oai_harvests = [
     # 26773 OAI Marineland - 18 items
     "collection_id": 26773,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "http://www.palosverdeshistory.org/oai2",
       "query_params": "pvld_4833"
     }
@@ -13,8 +12,7 @@ oai_harvests = [
     # 26774 OAI Palos Verdes Bulletin - 82 items
     "collection_id": 26774,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "http://www.palosverdeshistory.org/oai2",
       "query_params": "pvld_4048"
     }
@@ -23,8 +21,7 @@ oai_harvests = [
     # 26775 OAI Palos Verdes College Photo Collection - 225 items
     "collection_id": 26775,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "http://www.palosverdeshistory.org/oai2",
       "query_params": "pvld_4020"
     }
@@ -33,8 +30,7 @@ oai_harvests = [
     # 26776 OAI Phillips Ranch Photo Collection - 69 items
     "collection_id": 26776,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "http://www.palosverdeshistory.org/oai2",
       "query_params": "pvld_4017"
     }
@@ -43,8 +39,7 @@ oai_harvests = [
     # 26777 OAI Postcard Collection - 95 items
     "collection_id": 26777,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "http://www.palosverdeshistory.org/oai2",
       "query_params": "pvld_2"
     }
@@ -53,8 +48,7 @@ oai_harvests = [
     # 26778 OAI Your Story is the Peninsula's Story Collection - 90 items
     "collection_id": 26778,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "http://www.palosverdeshistory.org/oai2",
       "query_params": "pvld_4256"
     }
@@ -63,8 +57,7 @@ oai_harvests = [
     # 26779 OAI Malaga Cove Star Newspaper - 28 items
     "collection_id": 26779,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "http://www.palosverdeshistory.org/oai2",
       "query_params": "pvld_4406"
     }
@@ -73,74 +66,56 @@ oai_harvests = [
     # 26885 OAI Palos Verdes Homes Association Photo Collection - 1775 items
     "collection_id": 26885,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "http://www.palosverdeshistory.org/oai2",
       "query_params": "pvld_homes"
     }
   },
-  { # 27435: Sugoroku
+  {
+    # 27435: Sugoroku
     # 155 items
     # extra_data: metadataPrefix=marcxml&set=sugoroku
     'collection_id': 27435,
     'harvest_type': "oai",
-    'write_page': 0,
-    'oai': {
+    "harvest_data": {
         'url': "https://digicoll.lib.berkeley.edu/oai2d",
         'query_params': "metadataPrefix=marcxml&set=sugoroku"
     }
   },
-]
-
-'''
-oai_harvests = [
-  { # 2206: Big Pine Citizen Newspaper
+  {
+    # 2206: Big Pine Citizen Newspaper
     # 19 items
     # extra_data: big-pine-citizen-newspaper
     # curl 'http://oai.quartexcollections.com/lmudigitalcollections/oai2?verb=ListMetadataFormats'
     "collection_id": 2206,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "http://oai.quartexcollections.com/lmudigitalcollections/oai2",
       "query_params": "big-pine-citizen-newspaper"
     }
   },
-  { # 27435: Sugoroku
-    # 155 items
-    # extra_data: metadataPrefix=marcxml&set=sugoroku
-    'collection_id': 27435,
-    'harvest_type': "oai",
-    'write_page': 0,
-    'oai': {
-        'url': "https://digicoll.lib.berkeley.edu/oai2d",
-        'query_params': "metadataPrefix=marcxml&set=sugoroku"
-    }
-  },
-  { # 26693: Fritz-Metcalf
+  {
+    # 26693: Fritz-Metcalf
     # 8,729 items
     # extra_data: metadataPrefix=marcxml&set=fritz-metcalf
     # curl 'https://digicoll.lib.berkeley.edu/oai2d?verb=ListRecords&metadataPrefix=marcxml' > sample_data/fritz-metcalf.xm
     # <resumptionToken completeListSize="46849" cursor="0" expirationDate="2022-10-12T21:50:56Z">___AzfqSX</resumptionToken>
     "collection_id": 26693,
     "harvest_type": "oai",
-    "write_page": 0, 
-    "oai": {
+    "harvest_data": {
       "url": "https://digicoll.lib.berkeley.edu/oai2d",
       "query_params": "metadataPrefix=marcxml&set=fritz-metcalf"
     }
   },
-  { # 27934: San Francisco Gay Men's Chorus records, 2009-01
+  {
+    # 27934: San Francisco Gay Men's Chorus records, 2009-01
     # 47 items
     # extra_data: node:23758
-    # 
     "collection_id": 27934,
     "harvest_type": "oai",
-    "write_page": 0,
-    "oai": {
+    "harvest_data": {
       "url": "https://glbt.i8.dgicloud.com/oai/request",
       "query_params": "node:23758"
     }
   }
 ]
-'''
