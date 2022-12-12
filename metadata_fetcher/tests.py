68cf88f0541de52e24d548868a2c310fcdf95fbf
amywieliczka <amywieliczka@gmail.com>
2022-12-11 22:23:31 -0500

    test for one of each type, exclude nuxeo (no api key)

--- metadata_fetcher/tests.py
+++ metadata_fetcher/tests.py
@@ -1,12 +1,17 @@
 from lambda_function import fetch_collection
-from sample_data.nuxeo_harvests import *
-from sample_data.oac_harvests import *
-from sample_data.oai_harvests import *
+from sample_data.nuxeo_harvests import nuxeo_harvests, \
+    nuxeo_complex_object_harvests, nuxeo_nested_complex_object_harvests
+from sample_data.oac_harvests import oac_harvests
+from sample_data.oai_harvests import oai_harvests
 import json
 
-harvests = oac_harvests
-#  + nuxeo_complex_object_harvests + nuxeo_nested_complex_object_harvests
+harvests = [
+    # nuxeo_harvests[1], nuxeo_complex_object_harvests[0],
+    # nuxeo_nested_complex_object_harvests[0],
+    oac_harvests[0], oai_harvests[0]
+]
+
 for harvest in harvests:
     print(f"tests.py: {json.dumps(harvest)}")
-    fetch_collection(json.dumps(harvest), {})
-    print(f"Harvested: {str(harvest)}")
+    status = fetch_collection(json.dumps(harvest), {})
+    print(f"Harvest status: {status}")
