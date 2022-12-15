e5e3d13dcabf06c62e0a7bac634e5b8fc254e365
amywieliczka <amywieliczka@gmail.com>
2022-12-15 09:14:16 -0700

    update tests to take a debug flag

--- metadata_mapper/tests.py
+++ metadata_mapper/tests.py
@@ -1,19 +1,36 @@
-from lambda_function import map_page
+from lambda_shepherd import map_collection
 # from validate_mapping import validate_mapped_collection
-from sample_data.nuxeo_harvests import *
-from sample_data.oac_harvests import *
-from sample_data.islandora_harvests import *
+from sample_data.nuxeo_harvests import nuxeo_harvests, \
+    nuxeo_complex_object_harvests, nuxeo_nested_complex_object_harvests
+from sample_data.oac_harvests import oac_harvests
+from sample_data.islandora_harvests import islandora_harvests
 import json
+import argparse
+import logging
 
-harvests = islandora_harvests
+def main():
+    harvests = [
+        # oac_harvests[0],
+        islandora_harvests[0],
+        nuxeo_harvests[0],
+        nuxeo_complex_object_harvests[0],
+        nuxeo_nested_complex_object_harvests[0]
+    ]
 
-for harvest in harvests:
-    print(f"mapping tests: {json.dumps(harvest)}")
-    map_page(json.dumps(harvest), {})
-    print(f"mapped: {str(harvest)}")
+    for harvest in harvests:
+        print(f"mapping tests: {json.dumps(harvest)}")
+        map_collection(json.dumps(harvest), {})
+        print(f"mapped: {str(harvest)}")
 
-# for harvest in harvests:
-#     print(f"validate mapping: {json.dumps(harvest)}")
-#     validate_mapped_collection(json.dumps(harvest))
-#     print(f"validated: {str(harvest)}")
-    
+    # for harvest in harvests:
+    #     print(f"validate mapping: {json.dumps(harvest)}")
+    #     validate_mapped_collection(json.dumps(harvest))
+    #     print(f"validated: {str(harvest)}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument( '-log', '--loglevel', default='warning', help='log level (default: warning)')
+    args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel.upper())
+    logging.info('logging now set up')
+    main()
