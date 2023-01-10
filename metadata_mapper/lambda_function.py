65401c8f9982229dc8f09d3ced6f02c84e8444be
Tim Frazee <tim@castironcoding.com>
2023-01-09 16:52:37 -0800

    Refactor mapping to iterate over inherited UCLDC maps
    * Replaced #to_UCLDC implementation to iterate over #UCLDC_map method chain, creating mapped_metadata

--- metadata_mapper/lambda_function.py
+++ metadata_mapper/lambda_function.py
@@ -21,7 +21,8 @@ def import_vernacular_reader(mapper_type):
 
     mapper_module = importlib.import_module(
         f"mappers.{mapper_parent_modules}.{snake_cased_mapper_name}_mapper",
-        package="metadata_mapper")
+        package="metadata_mapper"
+    )
 
     mapper_type_words = snake_cased_mapper_name.split('_')
     class_type = ''.join([word.capitalize() for word in mapper_type_words])
@@ -29,7 +30,7 @@ def import_vernacular_reader(mapper_type):
         mapper_module, f"{class_type}Vernacular")
 
     if not issubclass(vernacular_class, Vernacular):
-        print(f"{ mapper_type } not a subclass of Vernacular")
+        print(f"{mapper_type} not a subclass of Vernacular")
         exit()
     return vernacular_class
 
@@ -58,7 +59,7 @@ def map_page(payload, context):
     vernacular_reader = import_vernacular_reader(payload.get('mapper_type'))
     source_vernacular = vernacular_reader(payload)
     api_resp = source_vernacular.get_api_response()
-    source_metadata_records = source_vernacular.parse(api_resp)
+    mapped_records = source_vernacular.parse(api_resp)
     collection = payload.get('collection', {})
 
     for enrichment_url in collection.get('rikolti__pre_mapping', []):
@@ -68,16 +69,12 @@ def map_page(payload, context):
         logging.debug(
             f"[{collection['id']}]: running enrichment: {enrichment_func} "
             f"for page {payload['page_filename']} with kwargs: {kwargs}")
-        source_metadata_records = [
+        mapped_records = [
             record.enrich(enrichment_func, **kwargs)
-            for record in source_metadata_records
+            for record in mapped_records
         ]
 
-    mapped_records = [record.to_UCLDC() for record in source_metadata_records]
     writer = UCLDCWriter(payload)
-    if settings.DATA_DEST == 'local':
-        writer.write_local_mapped_metadata(
-            [record.to_dict() for record in mapped_records])
 
     for enrichment_url in collection.get('rikolti__enrichments', []):
         enrichment_func, kwargs = parse_enrichment_url(enrichment_url)
--- metadata_mapper/mappers/oai/__init__.py
+++ metadata_mapper/mappers/oai/__init__.py
@@ -1,3 +1,3 @@
-from .oai_vernacular import OaiVernacular
+from .oai_mapper import OaiRecord, OaiVernacular
 from .cca_vault_mapper import CcaVaultRecord, CcaVaultVernacular
 from .islandora_mapper import IslandoraRecord, IslandoraVernacular
