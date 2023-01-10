f4c5ee1cc6d2b0180958915802f1d0d9a96eb0cb
Tim Frazee <tim@castironcoding.com>
2023-01-09 16:53:43 -0800

    Implement CCA Vault Mapper

--- metadata_mapper/mappers/oai/islandora_mapper.py
+++ metadata_mapper/mappers/oai/islandora_mapper.py
@@ -1,63 +1,9 @@
 import requests
 
-from ..mapper import Record
-from .oai_vernacular import OaiVernacular
+from .oai_mapper import OaiRecord, OaiVernacular
 
 
-class IslandoraRecord(Record):
-
-    def to_UCLDC(self):
-        self.mapped_data = {
-            'contributor': self.source_metadata.get('contributor'),
-            'creator': self.source_metadata.get('creator'),
-            'date': self.collate_fields([
-                "available",
-                "created",
-                "date",
-                "dateAccepted",
-                "dateCopyrighted",
-                "dateSubmitted",
-                "issued",
-                "modified",
-                "valid"
-            ]),
-            'description': self.collate_fields([
-                "abstract",
-                "description",
-                "tableOfContents"
-            ]),
-            'extent': self.source_metadata.get('extent'),
-            'format': self.collate_fields(["format", "medium"]),
-            'identifier': self.collate_fields(
-                ["bibliographicCitation", "identifier"]),
-            'is_shown_by': self.map_is_shown_by(),
-            'is_shown_at': self.map_is_shown_at(),
-            'provenance': self.source_metadata.get('provenance'),
-            'publisher': self.source_metadata.get('publisher'),
-            'relation': self.collate_fields([
-                "conformsTo",
-                "hasFormat",
-                "hasPart",
-                "hasVersion",
-                "isFormatOf",
-                "isPartOf",
-                "isReferencedBy",
-                "isReplacedBy",
-                "isRequiredBy",
-                "isVersionOf",
-                "references",
-                "relation",
-                "replaces",
-                "requires"
-            ]),
-            'rights': self.collate_fields(["accessRights", "rights"]),
-            'spatial': self.collate_fields(["coverage", "spatial"]),
-            'subject': self.map_subject(),
-            'temporal': self.source_metadata.get('temporal'),
-            'title': self.source_metadata.get('title'),
-            'type': self.source_metadata.get('type'),
-        }
-        return self
+class IslandoraRecord(OaiRecord):
 
     def map_subject(self):
         # https://github.com/calisphere-legacy-harvester/dpla-ingestion/blob/ucldc/lib/mappers/dublin_core_mapper.py#L117-L127
