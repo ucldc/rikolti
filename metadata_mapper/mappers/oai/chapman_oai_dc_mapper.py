3fe9e86e45136ac931f231181f7283b0feeb2dc4
Lucas Thurston <lucas@castironcoding.com>
2023-01-19 12:58:58 -0800

    Refactor chapman for new conventions

--- metadata_mapper/mappers/oai/chapman_oai_dc_mapper.py
+++ metadata_mapper/mappers/oai/chapman_oai_dc_mapper.py
@@ -3,32 +3,33 @@ from typing import Union
 from .oai_mapper import OaiRecord, OaiVernacular
 
 class ChapmanOaiDcRecord(OaiRecord):
+
+    def UCLDC_map(self):
+        return {
+            'description': self.collate_values([
+                self.source_metadata.get('abstract'),
+                self.map_description(),
+                self.source_metadata.get('tableOfContents')
+            ])
+        }
+
     def map_is_shown_at(self) -> Union[str, None]:
-        return self.identifier_for_image
+        return self.identifier_for_image()
 
     def map_is_shown_by(self) -> Union[str, None]:
-        if "type" not in self.source_metadata:
+        if not self.is_image_type():
             return
 
-        type: list[str] = self.source_metadata.get("type", [])
-
-        if not type or type[0].lower() != "image":
-            return
-
-        url: Union[str, None] = self.identifier_for_image
+        url: Union[str, None] = self.identifier_for_image()
 
         return f"{url.replace('items', 'thumbs')}?gallery=preview" if url else None
 
     def map_description(self) -> Union[str, None]:
-        breakpoint()
-        return "test"
-
         if 'description' not in self.source_metadata:
             return
 
         return [d for d in self.source_metadata.get('description') if 'thumbnail' not in d]
 
-    @property
     def identifier_for_image(self) -> Union[str, None]:
         if "identifier" not in self.source_metadata:
             return
