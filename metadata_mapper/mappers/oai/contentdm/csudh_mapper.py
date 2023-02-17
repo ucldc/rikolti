0fd2b5fe9049b5405dd797fb2b0202685a2b517f
Lucas Thurston <lucas@castironcoding.com>
2023-02-17 11:39:25 -0800

    Implement oai.contentdm.csudh

---
+++ metadata_mapper/mappers/oai/contentdm/csudh_mapper.py
@@ -0,0 +1,30 @@
+from ..contentdm_mapper import ContentdmRecord, ContentdmVernacular
+
+
+class CsudhRecord(ContentdmRecord):
+    def UCLDC_map(self):
+        return {
+            "identifier": self.map_identifier()
+        }
+
+    def map_identifier(self):
+        identifier = self.collate_fields(['bibliographicCitation', 'identifier'])
+
+        value = self.source_metadata.get("title", [])
+
+        return identifier + [t for t in value if t.startswith('csudh')]
+
+    def map_title(self):
+        if "title" not in self.source_metadata:
+            return
+
+        value = self.source_metadata.get("title")
+
+        if isinstance(value, (str, bytes)):
+            return value
+
+        return [t for t in value if not t.startswith('ark:') and not t.startswith('csudh')]
+
+
+class CsudhVernacular(ContentdmVernacular):
+    record_cls = CsudhRecord
