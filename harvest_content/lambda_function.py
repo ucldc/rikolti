ab7a7ea8ce1ce3faed9aca6ab331bc0522e2749a
amywieliczka <amywieliczka@gmail.com>
2023-02-24 16:29:45 -0800

    Add a ContentHarvester class

--- harvest_content/lambda_function.py
+++ harvest_content/lambda_function.py
@@ -12,77 +12,6 @@ class DownloadError(Exception):
     pass
 
 
-def download_source(http, content_source, auth=None):
-    '''
-        download source file to local disk
-    '''
-    if not content_source:
-        return None
-
-    filename = content_source.get('filename')
-    src_url = content_source.get('url')
-    mimetype = content_source.get('mimetype')
-
-    tmp_file_path = os.path.join('/tmp', filename)
-    print('downloading...')
-    if os.path.exists(tmp_file_path):
-        print(f"File already exists, using: {tmp_file_path}")
-        return tmp_file_path
-
-    # Weird how we have to use username/pass to hit this endpoint
-    # but we have to use auth token to hit API endpoint
-    request = {
-        "url": src_url,
-        "stream": True,
-        "timeout": (12.05, (60 * 10) + 0.05) # connect, read
-    }
-    if auth:
-        request['auth'] = auth
-
-    try:
-        response = http.get(**request)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        raise DownloadError(
-            f"ERROR: failed to download source file: {err}"
-        )
-
-    # print(response.headers)
-    # print(response.status_code)
-
-    with open(tmp_file_path, 'wb') as f:
-        for block in response.iter_content(1024):
-            f.write(block)
-
-    print(
-        f"Downloaded file from {src_url} to "
-        f"{tmp_file_path} - mimetype: {mimetype} "
-        f"- fsize: {os.path.getsize(tmp_file_path)}"
-    )
-    return tmp_file_path
-
-
-def create_requests_session():
-    retry_strategy = Retry(
-        total=3,
-        status_forcelist=[413, 429, 500, 502, 503, 504],
-    )
-    adapter = HTTPAdapter(max_retries=retry_strategy)
-    http = requests.Session()
-    http.mount("https://", adapter)
-    http.mount("http://", adapter)
-    return http
-
-
-def create_s3_session():
-    return boto3.client(
-        's3',
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name=settings.AWS_REGION
-    )
-
-
 def get_mapped_records(collection_id, page_filename) -> dict:
     if settings.DATA_SRC == 'local':
         print(f"Getting local mapped records for {collection_id}, {page_filename}")
@@ -120,101 +49,125 @@ def get_child_records(collection_id, parent_id):
     return mapped_child_records
 
 
-def harvest_media(http, calisphere_id, media_source):
-    media_source_filepath = None
-    media_filepath = None
-    # thumbnail_s3_path = None
+class ContentHarvester(object):
+    # context = {
+    #   'mapper_type': 'nuxeo.nuxeo', 
+    #   'collection_id': '12345', 
+    #   'page_filename': '1.jsonl'
+    # }
+    def __init__(self, context, src_auth=None):
+        self.http = requests.Session()
 
-    if media_source:
-        try:
-            media_source_filepath = download_source(
-                http,
-                media_source,
-                auth=(settings.NUXEO_USER, settings.NUXEO_PASS)
-            )
-        except DownloadError as err:
-            raise DownloadError(f"[{calisphere_id}]: {err}")
-
-    if media_source_filepath:
-        if media_source.get('nuxeo_type') == 'SampleCustomPicture':
-            media_filepath = derivatives.make_jp2(
-                media_source_filepath, media_source.get('mimetype'))
-            jp2_mimetype = 'image/jp2'
-        else:
-            print(f"not a jp2 sitch: {media_source_filepath}")
-            media_filepath = media_source_filepath
-
-    # if media_filepath:
-    #     media_s3_path = save_thumbnail(s3, media_filepath)
-
-    return media_filepath
-
-
-def harvest_thumbnail(http, calisphere_id, thumbnail_source):
-    thumbnail_source_filepath = None
-    thumbnail_filepath = None
-    # thumbnail_s3_path = None
-
-    if thumbnail_source:
-        try:
-            thumbnail_source_filepath = download_source(
-                http,
-                thumbnail_source,
-                auth=(settings.NUXEO_USER, settings.NUXEO_PASS)
-            )
-        except DownloadError as err:
-            raise DownloadError(f"[{calisphere_id}]: {err}")
-
-    if thumbnail_source_filepath:
-        try:
-            thumbnail_filepath = derivatives.make_thumbnail(
-                thumbnail_source_filepath,
-                thumbnail_source.get('mimetype')
-            )
-        except Exception as err:
-            raise Exception(f"[{calisphere_id}]: {err}")
-
-    # if thumbnail_filepath:
-    #     thumbnail_s3_path = save_thumbnail(s3, thumbnail_filepath)
-
-    return thumbnail_filepath
-
-
-def harvest_record_content(mapper_type, http, record, collection_id):
-    print(
-        f"[{collection_id}, {mapper_type}]: harvest record: "
-        f"{record.get('calisphere-id')}"
-    )
-    if mapper_type == 'nuxeo.nuxeo':
-        media = harvest_media(
-            http,
-            record.get('calisphere-id'),
-            record.get('media_source')
+        retry_strategy = Retry(
+            total=3,
+            status_forcelist=[413, 429, 500, 502, 503, 504],
         )
-        print(f"Media Path: {media}")
-        child_records = get_child_records(
-            collection_id,
-            record.get('calisphere-id')
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        self.http.mount("https://", adapter)
+        self.http.mount("http://", adapter)
+
+        self.s3 = boto3.client(
+            's3',
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            region_name=settings.AWS_REGION
         )
+        self.src_auth = src_auth
+        self.harvest_context = context
+
+    def harvest(self, record):
+        calisphere_id = record.get('calisphere-id')
+        collection_id = self.harvest_context.get('collection_id')
+
+        # Harvest Media File for Record
+        media_src = record.get('media_source')
+        media_src_file = self._download_src(media_src)
+        media_dest = None
+        if media_src_file:
+            if media_src.get('nuxeo_type') == 'SampleCustomPicture':
+                media_dest = derivatives.make_jp2(
+                    media_src_file, media_src.get('mimetype'))
+            else:
+                media_dest = {
+                    'media_filepath': media_src_file,
+                    'mimetype': media_src.get('mimetype')
+                }
+        print(f"Media Path: {media_dest}")
+
+        # Harvest Thumbnail File for Record
+        thumbnail_src = record.get('thumbnail_source')
+        thumbnail_src_file = self._download_src(thumbnail_src)
+        thumbnail_dest = None
+        if thumbnail_src_file:
+            thumbnail_dest = derivatives.make_thumbnail(
+                thumbnail_src_file,
+                thumbnail_src.get('mimetype')
+            )
+        print(f"Thumbnail Path: {thumbnail_dest}")
+
+        # Recurse through the record's children (if any)
+        child_records = get_child_records(collection_id, calisphere_id)
+        children_media = []
         for child in child_records:
-            harvest_record_content(mapper_type, http, child, collection_id)
+            children_media.append(self.harvest(child))
 
-    thumbnail = harvest_thumbnail(
-        http,
-        record.get('calisphere-id'),
-        record.get('thumbnail_source')
-    )
-    if thumbnail:
-        # update record
-        print(f"Thumbnail Path: {thumbnail}")
-    else:
-        warn_level = "ERROR"
-        if 'sound' in record.get('type', []):
-            warn_level = "WARNING"
+        content = {
+            'thumbnail': thumbnail_dest,
+        }
+        if media_dest:
+            content['media'] = media_dest
+        if children_media:
+            content['children'] = children_media
+        return content
+
+    def _download_src(self, content_src):
+        '''
+            download source file to local disk
+        '''
+        if not content_src:
+            return None
+
+        filename = content_src.get('filename')
+        src_url = content_src.get('url')
+        mimetype = content_src.get('mimetype')
+
+        tmp_file_path = os.path.join('/tmp', filename)
+        print('downloading...')
+        if os.path.exists(tmp_file_path):
+            print(f"File already exists, using: {tmp_file_path}")
+            return tmp_file_path
+
+        # Weird how we have to use username/pass to hit this endpoint
+        # but we have to use auth token to hit API endpoint
+        request = {
+            "url": src_url,
+            "stream": True,
+            "timeout": (12.05, (60 * 10) + 0.05) # connect, read
+        }
+        if self.src_auth:
+            request['auth'] = self.src_auth
+
+        try:
+            response = self.http.get(**request)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            raise DownloadError(
+                f"ERROR: failed to download source file: {err}"
+            )
+
+        # print(response.headers)
+        # print(response.status_code)
+
+        with open(tmp_file_path, 'wb') as f:
+            for block in response.iter_content(1024):
+                f.write(block)
+
         print(
-            f"{record.get('calisphere-id')}: {warn_level} - NO THUMBNAIL: "
-            f"{record.get('type')}"
+            f"Downloaded file from {src_url} to "
+            f"{tmp_file_path} - mimetype: {mimetype} "
+            f"- fsize: {os.path.getsize(tmp_file_path)}"
         )
+        return tmp_file_path
 
 
 # {"collection_id": 26098, "mapper_type": "nuxeo", "page_filename": "r-0"}
@@ -227,13 +180,31 @@ def harvest_page_content(payload, context):
         payload.get('collection_id'),
         payload.get('page_filename')
     )
+
+    collection_id = payload.get('collection_id')
     mapper_type = payload.get('mapper_type')
 
-    http = create_requests_session()
-    # s3 = create_s3_session()
+    auth = None
+    if mapper_type == 'nuxeo.nuxeo':
+        auth = (settings.NUXEO_USER, settings.NUXEO_PASS)
+    harvester = ContentHarvester(payload, auth=auth)
+
     for record in records:
-        harvest_record_content(
-            mapper_type, http, record, payload.get('collection_id'))
+        print(
+            f"[{collection_id}, {mapper_type}]: "
+            f"harvest record: {record.get('calisphere-id')}"
+        )
+
+        content = harvester.harvest(record)
+
+        if not content.get('thumbnail'):
+            warn_level = "ERROR"
+            if 'sound' in record.get('type', []):
+                warn_level = "WARNING"
+            print(
+                f"{record.get('calisphere-id')}: {warn_level} - NO THUMBNAIL: "
+                f"{record.get('type')}"
+            )
 
     return records
 
