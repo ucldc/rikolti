6bad8f1e0f2a6ab5b1281bb346d0bdf524cf559b
amywieliczka <amywieliczka@gmail.com>
2023-03-07 11:57:54 -0800

    Fully remove kakadu, use image magick to make jp2 conversion

--- content_harvester/content_harvester/derivatives.py
+++ content_harvester/content_harvester/derivatives.py
@@ -11,22 +11,6 @@ class ThumbnailError(Exception):
     pass
 
 
-# Settings recommended as a starting point by Jon Stroop.
-# See https://groups.google.com/forum/?hl=en#!searchin/iiif-discuss/kdu_compress/iiif-discuss/OFzWFLaWVsE/wF2HaykHcd0J
-KDU_COMPRESS_BASE_OPTS = [
-    "-quiet", "-rate",
-    "2.4,1.48331273,.91673033,.56657224,.35016049,.21641118,.13374944,"
-    ".08266171",
-    "Creversible=yes", "Clevels=7", "Cblk={64,64}", "Cuse_sop=yes",
-    "Cuse_eph=yes", "Corder=RLCP", "ORGgen_plt=yes", "ORGtparts=R",
-    "Stiles={1024,1024}", "-double_buffering", "10", "-num_threads", "4",
-    "-no_weights"
-]
-
-KDU_COMPRESS_DEFAULT_OPTS = KDU_COMPRESS_BASE_OPTS[:]
-KDU_COMPRESS_DEFAULT_OPTS.extend(["-jp2_space", "sRGB"])
-
-
 # decorator function
 def subprocess_exception_handler(func):
     def wrapper(*args, **kwargs):
@@ -185,64 +169,22 @@ def tiff_to_srgb_libtiff(input_path):
     return output_path
 
 
-# only used internally to this module
-# @subprocess_exception_handler
-# def uncompress_jp2000(compressed_path):
-#     ''' uncompress a jp2000 file using kdu_expand '''
-#     output_path = f"{compressed_path.split('.')[0]}.tiff"
-#     if os.path.exists(output_path):
-#         return output_path
-
-#     kdu_expand_location = CONTENT_PROCESSES['kdu_expand']
-#     process = [
-#         kdu_expand_location, "-i", compressed_path, "-o", output_path
-#     ]
-#     msg = (
-#         f"File uncompressed using kdu_expand. Input: "
-#         f"{compressed_path}, output: {output_path}"
-#     )
-#     subprocess.check_output(process, stderr=subprocess.STDOUT)
-#     print(msg)
-#     return output_path
-
-
-# only used internally to this module
 @subprocess_exception_handler
 def tiff_to_jp2(tiff_path):
-    ''' convert a tiff to jp2 using kdu_compress.
-    tiff must be uncompressed.'''
+    ''' convert a tiff to jp2 using image magick.'''
     jp2_path = f"{tiff_path.split('.')[0]}.jp2"
     if os.path.exists(jp2_path):
         return jp2_path
 
-    # kdu_compress_location = CONTENT_PROCESSES['kdu_compress']
-    # process = [kdu_compress_location, "-i", tiff_path, "-o", jp2_path]
-    # process.extend(KDU_COMPRESS_DEFAULT_OPTS)
-
     magick_location = CONTENT_PROCESSES['magick']
     process = [
-        magick_location, "-quiet", "-format", "-jp2", "-define", 
-        f"jp2:rate=10", f"{tiff_path}[0]", jp2_path
+        magick_location, "-quiet", "-format", "-jp2", "-define",
+        "jp2:rate=10", f"{tiff_path}[0]", jp2_path
     ]
     msg = f"{tiff_path} converted to {jp2_path}"
 
-    # try:
     subprocess.check_output(process, stderr=subprocess.STDOUT)
     print(msg)
-    # except subprocess.CalledProcessError as e:
-        # print(f"A kdu_compress command failed. Trying alternate:\n{e}")
-        # proc = [kdu_compress_location, "-i", tiff_path, "-o", jp2_path]
-        # proc.extend(KDU_COMPRESS_BASE_OPTS)
-        # try:
-        #     subprocess.check_output(process, stderr=subprocess.STDOUT)
-        #     print(msg)
-        # except subprocess.CalledProcessError as e:
-        #     print(
-        #         f"ERROR: `kdu_compress` failed: {e.cmd}\n"
-        #         f"returncode was: {e.returncode}\n"
-        #         f"output was: {e.output}"
-        #     )
-        #     return None
     return jp2_path
 
 
@@ -257,10 +199,7 @@ def make_jp2(source_file_path, mimetype):
     prepped_file_path = None
     if mimetype in ['image/jpeg', 'image/gif', 'image/png', 'image/tiff']:
         converted_file_path = tiff_conversion(source_file_path)
-        if converted_file_path:
-            prepped_file_path = tiff_to_srgb_libtiff(converted_file_path)
-    # elif mimetype in ['image/jp2', 'image/jpx', 'image/jpm']:
-    #     prepped_file_path = uncompress_jp2000(source_file_path)
+        prepped_file_path = tiff_to_srgb_libtiff(converted_file_path)
 
     if not prepped_file_path:
         print(
