395e08f90e75689ef5eb7a55fb335182f0ced141
Lucas Thurston <lucas@castironcoding.com>
2023-03-09 11:18:30 -0800

    Simplify Arck mapper
    After an analysis of Arck mapper data, it was
    determined that it could be simplified significantly.

--- metadata_mapper/mappers/oai/contentdm/arck_mapper.py
+++ metadata_mapper/mappers/oai/contentdm/arck_mapper.py
@@ -2,33 +2,14 @@ from ..contentdm_mapper import ContentdmRecord, ContentdmVernacular
 
 
 class ArckRecord(ContentdmRecord):
-    """
-    TODO: Analysis of the 11 collections to see if the `map_is_shown_by()` logic can be simplified.
-    """
-
     identifier_match = "view/ARCK3D"
 
     def UCLDC_map(self):
         return {
-            "source": self.source_metadata.get("source")
+            "source": self.source_metadata.get("source"),
+            "isShownBy": self.source_metadata.get("relation"),
+            "relation": ""
         }
-
-    def map_is_shown_by(self):
-        values = self.source_metadata.get("relation")
-        if not values:
-            return
-
-        candidates = [v for v in values if "/thumbnail" in v]
-        if not candidates:
-            return
-
-        return candidates[-1]
-
-    def map_relation(self):
-        if self.map_is_shown_by():
-            return
-
-        return self.source_metadata.get("relation")
 
 
 class ArckVernacular(ContentdmVernacular):
