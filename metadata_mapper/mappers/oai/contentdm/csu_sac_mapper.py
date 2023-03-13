786eba907d31448b6eb4066a1b635b9e81c23fbc
Lucas Thurston <lucas@castironcoding.com>
2023-03-13 16:13:08 -0700

    Implement oai.contentdm.csu_sac mapper

---
+++ metadata_mapper/mappers/oai/contentdm/csu_sac_mapper.py
@@ -0,0 +1,15 @@
+from ..contentdm_mapper import ContentdmRecord, ContentdmVernacular
+
+
+class CsuSacRecord(ContentdmRecord):
+    def UCLDC_map(self):
+        return {
+            'date': self.collate_fields([
+                'created',
+                'date'
+            ]),
+        }
+
+
+class CsuSacVernacular(ContentdmVernacular):
+    record_cls = CsuSacRecord
