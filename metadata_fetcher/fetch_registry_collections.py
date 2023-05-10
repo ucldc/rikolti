3cfe68d79556ebb047691d21311a11f90b3a8a91
amywieliczka <amywieliczka@gmail.com>
2023-05-09 17:00:04 -0700

    common module with registry_list generator function to iterate an api

--- metadata_fetcher/fetch_registry_collections.py
+++ metadata_fetcher/fetch_registry_collections.py
@@ -1,57 +1,48 @@
-import requests
 import argparse
-import json
 import sys
 import lambda_function
 import logging
 
+import sys
+sys.path.append('../')
+from common.rikolti_utilities import registry_list
 
 def fetch_endpoint(url, limit=None):
-
-    collection_page = url
     results = []
+    for collection in registry_list(url, limit):
+        log_msg = f"[{collection['collection_id']}]: " + "{}"
+        print(log_msg.format(
+            f"Fetching collection {collection['collection_id']} - "
+            f"{collection['solr_count']} items in solr as of "
+            f"{collection['solr_last_updated']}"
+        ))
+        logging.debug(log_msg.format(f"lambda payload: {collection}"))
+        return_val = lambda_function.fetch_collection(
+            collection, None)
+        results.append(return_val)
 
-    while collection_page and (not limit or len(results) < limit):
-        response = requests.get(url=collection_page)
-        response.raise_for_status()
-        total_collections = response.json().get('meta', {}).get('total_count', 1)
-        print(
-            f">>> Fetching {total_collections} collections "
-            f"described at {collection_page}"
-        )
-
-        collection_page = response.json().get('meta', {}).get('next')
-        if collection_page:
-            collection_page = f"https://registry.cdlib.org{collection_page}"
-        logging.debug(f"Next page: {collection_page}")
-        collections = response.json().get('objects', [response.json()])
-        for collection in collections:
-            if limit and len(results) >= limit:
-                break
-            log_msg = f"[{collection['collection_id']}]: " + "{}"
-            print(log_msg.format(
-                f"Fetching collection {collection['collection_id']} - "
-                f"{collection['solr_count']} items in solr as of "
-                f"{collection['solr_last_updated']}"
-            ))
-            logging.debug(log_msg.format(f"lambda payload: {collection}"))
-            return_val = lambda_function.fetch_collection(
-                collection, None)
-            results.append(return_val)
-
-            if return_val['statusCode'] != 200:
-                print(log_msg.format(f"Error: {return_val}"))
-            else:
-                print(log_msg.format(f"Fetch successful: {return_val}"))
-        collection_page = False
+        if return_val['statusCode'] != 200:
+            print(log_msg.format(f"Error: {return_val}"))
+        else:
+            print(log_msg.format(f"Fetch successful: {return_val}"))
 
     print(results)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Run fetcher for registry endpoint")
-    parser.add_argument('endpoint', help='registry api endpoint')
-    args = parser.parse_args(sys.argv[1:])
-    fetch_endpoint(args.endpoint)
+        description='Run fetcher for registry endpoing')
+    parser.add_argument(
+        'endpoint', help='registry api endpoint or collection id')
+    parser.add_argument(
+        '--limit', help='limit the number of collections to fetch', type=int)
+    args = parser.parse_args()
+
+    if args.endpoint.isdigit():
+        args.endpoint = (
+            f"https://registry.cdlib.org/api/v1/rikoltifetcher/{args.endpoint}/"
+            "?format=json"
+        )
+
+    fetch_endpoint(args.endpoint, args.limit)
     sys.exit(0)
