468d71f9a4076b020cb925199cb8c74ebbb5c115
amywieliczka <amywieliczka@gmail.com>
2023-06-26 09:54:23 -0700

    lint fix record_indexer, textract

--- textract/start_textract.py
+++ textract/start_textract.py
@@ -28,12 +28,12 @@ def lambda_handler(payload, context):
 					}
 				},
 				NotificationChannel={
-					'SNSTopicArn': "arn:aws:sns:us-west-2:563907706919:AmazonTextractPachamama",
+					'SNSTopicArn': "arn:aws:sns:us-west-2:563907706919:AmazonTextractPachamama",  # noqa: 501
 					'RoleArn': "arn:aws:iam::563907706919:role/TextractRole"
 				},
 				OutputConfig={
 					"S3Bucket": "rikolti",
-					"S3Prefix": f"textract/raw-output/{collection_id}/{calisphere_id}/textract-raw"
+					"S3Prefix": f"textract/raw-output/{collection_id}/{calisphere_id}/textract-raw"  # noqa: 501
 				}
 			)
 			print({'calisphere-id': key, 'job_id': textract_job})
