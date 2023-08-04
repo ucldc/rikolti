cc2ecd2ea41c4f9f9c955c9c7528d52248ce8547
Amy Wieliczka <amywieliczka@gmail.com>
2023-08-04 00:06:56 -0700

    change regex for Previous Identifier; split_description overload;
    rather than copying and moving fields out of description all in
    one function with finicky ordering properties, use separate
    search-only and substitute-only functions

--- metadata_mapper/mappers/flickr/sppl_mapper.py
+++ metadata_mapper/mappers/flickr/sppl_mapper.py
@@ -4,99 +4,63 @@ import re
 
 class SpplRecord(FlickrRecord):
     def UCLDC_map(self):
-        split_description = self.split_description()
         return {
-            "description": [split_description.get("description")],
-            "identifier": self.map_identifier(split_description),
-            "type": split_description.get("type"),
-            "rights": [split_description.get("rights_information")],
-            "provenance": [split_description.get("provenance")],
-            "subject": self.map_subject(split_description),
-            "date": split_description.get("date")
+            "description": [self.map_description()],
+            "identifier": self.map_identifier(),
+            "type": self.search_description(r"Type:([^\n]+)\n\s*\n"),
+            "rights": [self.search_description(r"Rights Information:([\S\s]+)")],
+            "provenance": [self.search_description(r"Source:([^\n]+)\n\s*\n")],
+            "subject": self.map_subject(),
+            "date": self.search_description(r"Date:([^\n]+)\n\s*\n")
         }
 
     @property
     def source_description(self):
         return self.source_metadata.get("description", {}).get("_content")
 
-    @staticmethod
-    def get_mapping_configuration():
-        """
-        The `rights_information` regex is a catch all that will capture
-        everything after "Rights Information:" appears in the description,
-        which is why it is last.
-        """
-        return [
-            {
-                "key": "type",
-                "regex": r"Type:([^\n]+)\n\s*\n"
-            },
-            {
-                "key": "provenance",
-                "regex": r"Source:([^\n]+)\n\s*\n"
-            },
-            {
-                "key": "date",
-                "regex": r"Date:([^\n]+)\n\s*\n"
-            },
-            {
-                "key": "identifier",
-                "regex": r"Identifier:([^\n]+)\n\s*\n"
-            },
-            {
-                "key": "owner",
-                "regex": r"Owner:([^\n]+)\n\s*\n",
-                "discard": True
-            },
-            {
-                "key": "previous_identifier_discard",
-                "regex": r"Previous Identifier: *N/A\n\s*\n",
-                "discard": True,
-                "keep_in_description": True
-            },
-            {
-                "key": "previous_identifier",
-                "regex": r"Previous Identifier:([^\n]+)\n\s*\n"
-            },
-            {
-                "key": "category",
-                "regex": r"Category:([^\n]+)\n\s*\n"
-            },
-            {
-                "key": "rights_information",
-                "regex": r"Rights Information:([\S\s]+)"
-            }
-        ]
+    def map_description(self):
+        description = self.source_description
+        description = re.sub(r"Type:([^\n]+)\n\s*\n", "", description, count=1)
+        description = re.sub(r"Source:([^\n]+)\n\s*\n", "", description, count=1)
+        description = re.sub(r"Date:([^\n]+)\n\s*\n", "", description, count=1)
+        description = re.sub(r"Identifier:([^\n]+)\n\s*\n", "", description, count=1)
+        description = re.sub(r"Previous Identifier:(.+\n?(?!\n).*)\n\s*\n", "", description, count=1)
+        description = re.sub(r"Category:([^\n]+)\n\s*\n", "", description, count=1)
+        description = re.sub(r"Rights Information:([\S\s]+)", "", description, count=1)
+        return description
 
-    def map_subject(self, split_description):
+    def search_description(self, regex):
+        matches = re.search(regex, self.source_description, re.MULTILINE)
+        if matches:
+            return matches.groups()[-1].strip()
+
+    def map_subject(self):
         subjects = [{"name": tag.get("raw")} for tag
                     in self.source_metadata.get("tags", {}).get("tag", [])]
 
-        category = split_description.get("category")
+        category = self.search_description(r"Category:([^\n]+)\n\s*\n")
 
         if category:
             subjects.append({"name": category.lower()})
 
         return subjects
 
-    def map_identifier(self, split_description):
+    def map_identifier(self):
         """
         Combine `previous_identifier` and `identifier` values from description
-        metadata. The `previous_identifier` may contain an ARK, which we
-        extract.
+        metadata. 
         """
-        previous_identifiers = split_description.get("previous_identifier", "")
-        identifiers = [split_description.get("identifier")]
+        previous_identifiers = self.search_description(
+            r"Previous Identifier:(.+\n?(?!\n).*)\n\s*\n") or []
+        if previous_identifiers:
+            previous_identifiers = previous_identifiers.replace("N/A", "")
+            previous_identifiers = re.split(r"\s+/\s+", previous_identifiers)
 
-        for previous_identifier in previous_identifiers.split(" / "):
-            if "ark:" in previous_identifier:
-                matches = re.search(r"ark:[\/a-z0-9]+", previous_identifier)
-                if matches:
-                    identifiers.append(matches[0])
-            else:
-                identifiers.append(previous_identifier)
+        identifiers = self.search_description(r"Identifier:([^\n]+)\n\s*\n")
+        if identifiers:
+            previous_identifiers.append(identifiers)
 
-        return [i for i in identifiers if i != "N/A"]
+        return [i for i in previous_identifiers if i != "N/A"]
 
 
 class SpplVernacular(FlickrVernacular):
