b104bc42e9224714486049707b348562f55604dd
Amy Wieliczka <amywieliczka@gmail.com>
2023-08-09 14:35:02 -0700

    Custom validator shims harvest-id for more accurate comparisons

--- metadata_mapper/mappers/oai/cca_vault_mapper.py
+++ metadata_mapper/mappers/oai/cca_vault_mapper.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from .oai_mapper import OaiRecord, OaiVernacular
+from ..mapper import Validator
 
 
 class CcaVaultRecord(OaiRecord):
@@ -31,5 +32,53 @@ class CcaVaultRecord(OaiRecord):
         return identifier[0] if identifier else None
 
 
+class CcaVaultValidator(Validator):
+
+    def generate_keys(self, collection: list[dict], type: str = None,
+                      context: dict = {}) -> dict[str, dict]:
+        """
+        Given a list of records, generates keys and returns a dict with the
+        original list contents as values.
+
+        This can be used to override the creation of keys for a given
+        mapper to ensure that key intersection works in the
+        validate_mapping module.
+
+        Parameters:
+            collection: list[dict]
+                Data to be added to resulting dict
+            type: str (default: None)
+                Optional context. Usually this will be used to tell
+                this method if it's dealing with Rikolti or Solr data.
+            context: dict (default: {})
+                Any information needed to perform these calculations.
+
+        Returns: dict[str, dict]
+            dict of dicts, keyed to ensure successful intersection.
+        """
+        if type == "Rikolti":
+            # "oai:vault.cca.edu:" is new style, 3433 uses it
+            # "oai:cca:" is old style, 26470, 3767, 26391 use it
+            # example: 3433--oai:vault.cca.edu:006f6694-a826-6c83-b455-5b62a1ef9d69/1
+            # vs: 26470--oai:cca:0029e39d-c3f3-9346-a7d8-6565486a75a8/1
+            if context.get('collection_id') == '3433':
+                # do the usual thing
+                return {
+                    f"{context.get('collection_id')}--{r['calisphere-id']}": r
+                    for r in collection
+                }
+            else:
+                shimmed_ids = {}
+                for r in collection:
+                    item_id = r['calisphere-id'].split(':')[-1]
+                    rikolti_id = (
+                        f"{context.get('collection_id')}--oai:cca:{item_id}"
+                    )
+                    shimmed_ids[rikolti_id] = r
+                return shimmed_ids
+        elif type == "Solr":
+            return {r['harvest_id_s']: r for r in collection}
+
 class CcaVaultVernacular(OaiVernacular):
     record_cls = CcaVaultRecord
+    validator = CcaVaultValidator
