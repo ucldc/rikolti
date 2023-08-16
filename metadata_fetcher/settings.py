b5ac59cd1ecab4b7ca57fae291ff8714f584a8ed
Amy Wieliczka <amywieliczka@gmail.com>
2023-08-16 16:13:07 -0700

    Removed metadata_fetcher LOG_LEVEL constant

--- metadata_fetcher/settings.py
+++ metadata_fetcher/settings.py
@@ -7,7 +7,6 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 DATA_DEST = os.environ.get('FETCHER_DATA_DEST', 's3')
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')    # doesn't currently do anything
 S3_BUCKET = os.environ.get('S3_BUCKET', False)
 NUXEO_TOKEN = os.environ.get('NUXEO')
 FLICKR_API_KEY = os.environ.get('FLICKR_API_KEY')
