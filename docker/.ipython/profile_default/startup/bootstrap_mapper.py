b7ff517ec78786b3f860313460c5c6047f557c82
Lucas Thurston <lucas@castironcoding.com>
2023-08-31 10:39:43 -0700

    Update docker env set up

--- docker/.ipython/profile_default/startup/bootstrap_mapper.py
+++ docker/.ipython/profile_default/startup/bootstrap_mapper.py
@@ -3,14 +3,14 @@
 import os
 import glob
 import importlib
-# from pprint import pprint
+import sys
 
 get_ipython().run_line_magic("load_ext", "autoreload")  # noqa: F821
 get_ipython().run_line_magic("alias_magic", "r %autoreload")  # noqa: F821
 
 # import all modules in the current cwd excluding some
 excludes = ["tests", "utilities", "settings", "__init__"]
-paths = glob.glob(f"{os.getcwd()}/*.py")
+paths = glob.glob(f"{os.getcwd()}/metadata_mapper/*.py")
 
 for path in paths:
     if any([exclude in path for exclude in excludes]):
@@ -18,4 +18,4 @@ for path in paths:
 
     module = os.path.basename(path)[:-3]
 
-    locals()[module] = importlib.import_module(module)
+    locals()[module] = importlib.import_module(f"metadata_mapper.{module}")
