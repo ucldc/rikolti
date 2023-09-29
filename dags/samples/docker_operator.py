a1b7bc72f840c26cd49c59f43cb247e59bf9ef05
Amy Wieliczka <amywieliczka@gmail.com>
2023-09-29 16:24:35 -0700

    Sample DockerOperator mounts

--- dags/samples/docker_operator.py
+++ dags/samples/docker_operator.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from docker.types import Mount
 from airflow.decorators import dag
 from airflow.models.param import Param
 from airflow.providers.docker.operators.docker import DockerOperator
@@ -41,5 +42,27 @@ def sample_docker_operators():
         mount_tmp_dir=False,
     )
     parameterize_print_task
+
+    # https://docker-py.readthedocs.io/en/stable/api.html#docker.types.Mount
+    # since the docker daemon is running on the host machine, we can mount
+    # a folder from the host machine into the container, but we cannot mount
+    # a folder from the airflow container machine into the container
+    mount_folder_task = DockerOperator(
+        task_id="mount_folder_task",
+        image="simple_python:latest",
+        container_name="mount_folder_task_container",
+        command="--message '{{ params.message }}' --output /tmp/rikolti_data/output.txt",
+        network_mode="bridge",
+        auto_remove='force',
+        mount_tmp_dir=False,
+        mounts=[
+            Mount(
+                source="/Users/awieliczka/Projects/rikolti_data",
+                target="/tmp/rikolti_data",
+                type="bind",
+            )
+        ]
+    )
+    mount_folder_task
 
 sample_docker_operators()
--- dags/samples/simple_python_image/hello.py
+++ dags/samples/simple_python_image/hello.py
@@ -4,6 +4,14 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--message', type=str, help='Message to print')
+    parser.add_argument('--output', type=str, help='Path to output file')
     args = parser.parse_args()
     message = args.message or "Hello from docker container!"
-    print(message)
+    
+    if args.output:
+        with open(args.output, 'w') as f:
+            f.write(message)
+            f.close()
+        print(f"Message written to {args.output}")
+    else:
+        print(message)
