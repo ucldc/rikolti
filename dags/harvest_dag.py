bfb99b31c5ed6e51e98ec25f8ef4a74e665e3070
Amy Wieliczka <amywieliczka@gmail.com>
2023-09-29 13:13:48 -0700

    Mistakenly committed hardcoded collection ID

--- dags/harvest_dag.py
+++ dags/harvest_dag.py
@@ -99,7 +99,7 @@ def get_mapping_status_task(collection: dict, mapped_pages: list):
     schedule=None,
     start_date=datetime(2023, 1, 1),
     catchup=False,
-    params={'collection_id': Param(3433, description="Collection ID to map")},
+    params={'collection_id': Param(None, description="Collection ID to map")},
     tags=["rikolti"],
 )
 def harvest():
