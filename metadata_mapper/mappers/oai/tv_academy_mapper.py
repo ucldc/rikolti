abaada0a03dd97b05a448d1cc534c2225545907d
Lucas Thurston <lucas@castironcoding.com>
2023-10-05 15:31:24 -0700

    Address tv_academy validation issues

--- metadata_mapper/mappers/oai/tv_academy_mapper.py
+++ metadata_mapper/mappers/oai/tv_academy_mapper.py
@@ -3,10 +3,11 @@ from .oai_mapper import OaiRecord, OaiVernacular
 
 class TvAcademyRecord(OaiRecord):
 
-    def UCLDC_map(self):
+    def UCLDC_map(self) -> dict:
         return {
-            'is_shown_at': self.first_string_in_field('identifier'),
-            'is_shown_by': self.first_string_in_field('relation')
+            'isShownAt': self.first_string_in_field('identifier'),
+            'isShownBy': self.first_string_in_field('relation'),
+            "relation": None
         }
 
 
