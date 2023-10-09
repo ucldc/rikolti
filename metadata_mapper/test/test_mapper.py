0e8042023731324ad12e7dc788f58795fc0daee4
Tim Frazee <tim@castironcoding.com>
2023-10-09 10:51:04 -0700

    Implement basic contentdm mapper test

--- metadata_mapper/test/test_mapper.py
+++ metadata_mapper/test/test_mapper.py
@@ -1,9 +1,9 @@
 import os
 import pytest
 
-from fixture_generator import FixtureGenerator
+from unittest import mock
 
-from metadata_mapper.mappers.oai.islandora_mapper import IslandoraRecord
+from .fixtures.fixture_generator import FixtureGenerator
 
 @pytest.fixture
 def test_data(generator_class = FixtureGenerator):
@@ -11,26 +11,33 @@ def test_data(generator_class = FixtureGenerator):
     data = generator.generate()
     yield data
 
-def test_islandora_mapper(test_data):
-    r = IslandoraRecord(test_data["collection_id"], test_data)
+def test_contentdm_mapper(test_data):
+    from ..mappers.oai.content_dm.contentdm_mapper import ContentdmRecord
+
+    r = ContentdmRecord(test_data.get("collection_id") or 123, test_data)
+    r.legacy_couch_db_id = "asdf--asdf"
     r.to_UCLDC()
 
 # This will loop through all mappers that don't have explicit test methods and
 # run them with default data
-def test_other_mappers(test_data):
+def xtest_other_mappers(test_data):
   for dir in os.scandir("../mappers"):
      if dir.is_dir():
         for file in os.scandir(dir.path):
            if file.name.endswith("_mapper.py"):
               name = file.name.replace("_mapper.py", "")
-              if f"test_{name}_mapper" in list(locals().keys):
+              if f"test_{name}_mapper" in list(locals().keys()):
+                 print(f"Running mapper-specific test for {name}")
                  continue
 
               package = locals().get(f"{name}_mapper")
+              breakpoint()
               if not package:
+                print(f"Mapper package not found for {name}")
                 continue
 
               camelized_name = ''.join(name.capitalize() or "_" for x in name.split("_"))
               record_class = package.getattr(f"{camelized_name}Record")
               r = record_class(test_data["collection_id"], test_data)
+              print(f"OMG WE DID IT {name}")
               r.to_UCLDC()
