bca49429d9dfac084b61a057404eef5b778ab5e4
amy wieliczka <amywieliczka@gmail.com>
2023-10-10 16:55:48 -0700

    update function signature for create_collection_validation_csv

--- metadata_mapper/validate_mapping.py
+++ metadata_mapper/validate_mapping.py
@@ -109,11 +109,10 @@ def validate_page(collection_id: int, page_id: int,
     return validator
 
 
-def create_collection_validation_csv(collection_id: int, **options) -> None:
+def create_collection_validation_csv(collection_id: int, **options) -> tuple[int, str]:
     result = validate_collection(collection_id, **options)
     filename = result.log.output_csv_to_bucket(collection_id)
-    return f"Output {len(result.log.log)} rows to {filename}"
-
+    return len(result.log.log), filename
 
 ## Private-ish
 
@@ -270,5 +269,6 @@ if __name__ == "__main__":
     print(f"Generating validations for collection {args.collection_id} with options:")
     print(kwargs)
 
-    collection_report = create_collection_validation_csv(args.collection_id, **kwargs)
-    print(collection_report)
+    num_rows, file_location = create_collection_validation_csv(
+        args.collection_id, **kwargs)
+    print(f"Output {num_rows} rows to {file_location}")
