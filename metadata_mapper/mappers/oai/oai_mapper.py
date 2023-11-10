a3891248120f35c909d288fd51744398d4f88d22
Lucas Thurston <lucas@castironcoding.com>
2023-11-10 12:24:21 -0800

    Map common fields in oai base

--- metadata_mapper/mappers/oai/oai_mapper.py
+++ metadata_mapper/mappers/oai/oai_mapper.py
@@ -9,10 +9,18 @@ from ..mapper import Record, Vernacular
 class OaiRecord(Record):
     """Superclass for OAI metadata."""
 
+    TYPES = {
+        "img": "image",
+        "txt": "text"
+    }
+
+    TYPES_BASEURL = "http://id.loc.gov/vocabulary/resourceTypes/"
+
     def UCLDC_map(self):
         return {
             # `legacy_couch_db_id` is set by a premapping function
             "calisphere-id": self.legacy_couch_db_id.split("--")[1],
+            "alternativeTitle": self.map_alternative_title,
             "isShownAt": self.map_is_shown_at,
             "isShownBy": self.map_is_shown_by,
             "contributor": self.source_metadata.get("contributor"),
@@ -33,6 +41,7 @@ class OaiRecord(Record):
             "extent": self.source_metadata.get("extent"),
             "format": self.collate_fields(["format", "medium"]),
             "identifier": self.collate_fields(["bibliographicCitation", "identifier"]),
+            "language": self.map_language,
             "provenance": self.source_metadata.get("provenance"),
             "publisher": self.source_metadata.get("publisher"),
             "relation": self.collate_fields([
@@ -52,13 +61,29 @@ class OaiRecord(Record):
                 "require"
             ]),
             "rights": self.collate_fields(["accessRights", "rights"]),
-            
+            "source": self.source_metadata.get("source"),
             "spatial": self.collate_fields(["coverage", "spatial"]),
             "subject": self.map_subject,
             "temporal": self.source_metadata.get("temporal"),
             "title": self.source_metadata.get("title"),
             "type": self.source_metadata.get("type")
         }
+
+    def map_alternative_title(self) -> list:
+        value = self.source_metadata.get("alternative", [])
+
+        if isinstance(value, list):
+            return value
+
+        return [value]
+
+    def map_is_shown_at(self):
+        """Will be implemented by child mapper classes"""
+        return
+
+    def map_is_shown_by(self):
+        """Will be implemented by child mapper classes"""
+        return
 
     def map_subject(self) -> Union[list[dict[str, str]], None]:
         # https://github.com/calisphere-legacy-harvester/dpla-ingestion/blob/ucldc/lib/mappers/dublin_core_mapper.py#L117-L127 # noqa: E501
@@ -70,13 +95,39 @@ class OaiRecord(Record):
             value = [value]
         return [{"name": v} for v in value if v]
 
-    def map_is_shown_at(self):
-        """Will be implemented by child mapper classes"""
-        return
 
-    def map_is_shown_by(self):
-        """Will be implemented by child mapper classes"""
-        return
+    def map_language(self) -> list:
+        value = self.source_metadata.get("language")
+
+        if isinstance(value, list):
+            return value
+
+        return [value]
+
+    def map_type(self) -> list:
+        """
+        Iterates the `type` values. If they are part of the controlled vocabulary
+        defined by `TYPES_BASEURL`, then they are mapped to the HR value.
+
+        Returns:
+            list
+        """
+        types = self.source_metadata.get("type", [])
+        if isinstance(types, str):
+            types = [types]
+
+        type_list = []
+        for t in types:
+            mapped_type = None
+            for (type_k, type_v) in self.TYPES.items():
+                if mapped_type:
+                    break
+                if t == f"{self.TYPES_BASEURL}{type_k}":
+                    mapped_type = type_v
+            if not mapped_type:
+                mapped_type = t.replace(self.TYPES_BASEURL, "")
+            type_list.append(mapped_type.lower())
+        return type_list
 
 
 class OaiVernacular(Vernacular):
