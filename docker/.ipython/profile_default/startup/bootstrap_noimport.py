2c932fdb0e7b44b243337fbefd417315a931eef8
Lucas Thurston <lucas@castironcoding.com>
2023-12-02 07:23:49 -0800

    Read .env in bootstrap scripts

--- docker/.ipython/profile_default/startup/bootstrap_noimport.py
+++ docker/.ipython/profile_default/startup/bootstrap_noimport.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
 
+from dotenv import load_dotenv
+
+load_dotenv()
 get_ipython().run_line_magic("load_ext", "autoreload")  # noqa: F821
 get_ipython().run_line_magic("alias_magic", "r %autoreload")  # noqa: F821
