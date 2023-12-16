7fc887a710f8f47cc277409442fe69a3b47dfb15
amy wieliczka <amywieliczka@gmail.com>
2023-12-16 13:57:53 -0800

    Removed defunct env variables, settings variables

--- record_indexer/settings.py
+++ record_indexer/settings.py
@@ -1,26 +1,11 @@
 import os
 
-from urllib.parse import urlparse
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATA_SRC_URL = os.environ.get("INDEXER_DATA_SRC", "file:///tmp/")
-DATA_SRC = {
-    "STORE": urlparse(DATA_SRC_URL).scheme,
-    "BUCKET": urlparse(DATA_SRC_URL).netloc,
-    "PATH": urlparse(DATA_SRC_URL).path,
-}
-
 ENDPOINT = os.environ.get("RIKOLTI_ES_ENDPOINT")
 AUTH = ("rikolti", os.environ.get("RIKOLTI_ES_PASS"))
-
-
-def local_path(collection_id, folder):
-    local_path = os.sep.join([DATA_SRC["PATH"], str(collection_id), folder])
-    return local_path
-
 
 RIKOLTI_HOME = os.environ.get("RIKOLTI_HOME", "/usr/local/airflow/dags/rikolti")
 RECORD_INDEX_CONFIG = os.sep.join(
