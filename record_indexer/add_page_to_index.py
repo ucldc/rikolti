dbf509691c31d837844decd22b301cea569c8485
amy wieliczka <amywieliczka@gmail.com>
2023-12-18 14:45:28 -0800

    update error handling, output

--- record_indexer/add_page_to_index.py
+++ record_indexer/add_page_to_index.py
@@ -1,5 +1,7 @@
 import json
 
+from pprint import pprint
+
 import requests
 
 from . import settings
@@ -14,9 +16,27 @@ def bulk_add(records: list, index: str):
     headers = {"Content-Type": "application/json"}
 
     r = requests.post(url, headers=headers, data=data, auth=settings.AUTH)
+    r.raise_for_status()
 
-    if r.status_code != 200:
-        raise requests.HTTPError(r.status_code, r.json())
+    bulk_resp = r.json()
+    if bulk_resp.get('errors') == True:
+        error_reasons = []
+        errors = []
+        for bulk_item in bulk_resp.get('items'):
+            for action, action_resp in bulk_item.items():
+                if 'error' in action_resp:
+                    error_reasons.append(action_resp['error'].get('reason'))
+                    errors.append(bulk_item)
+
+        error_reasons = list(set(error_reasons))
+        if len(error_reasons) > 1:
+            pprint(errors)
+        raise(
+            Exception(
+                f"{len(errors)} errors in bulk indexing "
+                f"{len(records)} records: {error_reasons}"
+            )
+        )
 
 
 def build_bulk_request_body(records: list, index: str):
@@ -32,15 +52,14 @@ def build_bulk_request_body(records: list, index: str):
     return body
 
 
-def flag_and_remove_unexpected_fields(record: dict, expected_fields: list):
-    calisphere_id = record.get("calisphere-id", None)
+def remove_unexpected_fields(record: dict, expected_fields: list):
+    removed_fields = []
     for field in list(record.keys()):
         if field not in expected_fields:
-            print(f"unexpected field `{field}` found in record `{calisphere_id}`")
-            print("   removing field from record")
+            removed_fields.append(field)
             record.pop(field)
 
-    return record
+    return removed_fields
 
 
 def get_expected_fields():
@@ -58,9 +77,28 @@ def add_page(version_page: str, index: str):
         records = get_with_content_urls_page_content(version_page)
 
     expected_fields = get_expected_fields()
+    flag_removed_fields = {}
     for record in records:
-        record = flag_and_remove_unexpected_fields(record, expected_fields)
+        removed_fields = remove_unexpected_fields(record, expected_fields)
+        calisphere_id = record.get("calisphere-id", None)
+        for field in removed_fields:
+            if field in flag_removed_fields:
+                flag_removed_fields[field].append(calisphere_id)
+            else:
+                flag_removed_fields[field] = [calisphere_id]
 
     bulk_add(records, index)
 
-    print(f"added page `{page}` to index `{index}`")
+    print(
+        f"added {len(records)} records to index `{index}` from "
+        f"page `{version_page}`"
+    )
+    for field, calisphere_ids in flag_removed_fields.items():
+        if len(calisphere_ids) != len(records):
+            print(
+                f"    {len(calisphere_ids)} items had {field} "
+                f"removed: `{calisphere_ids}`"
+            )
+        else:
+            print(f"    all {len(records)} records had {field} field removed")
+
