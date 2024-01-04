19e4b3542edd9fa48976e752ce1d6427f666ecea
amy wieliczka <amywieliczka@gmail.com>
2024-01-04 14:04:59 -0800

    pspl validation: ignore http vs. https differences

--- metadata_mapper/mappers/oai/pspl_mapper.py
+++ metadata_mapper/mappers/oai/pspl_mapper.py
@@ -1,4 +1,8 @@
+from typing import Any
+
+
 from .oai_mapper import OaiRecord, OaiVernacular
+from ..mapper import Validator
 
 
 class PsplRecord(OaiRecord):
@@ -20,5 +24,40 @@ class PsplRecord(OaiRecord):
         return f"https://collections.accessingthepast.org/cgi-bin/{collection_id}?a=d&d={record_id}"
 
 
+class PsplValidator(Validator):
+    def setup(self):
+        self.add_validatable_fields(
+            {
+                "field": "is_shown_at",
+                "validations": [
+                    Validator.required_field,
+                    PsplValidator.str_match_ignore_url_protocol,
+                    Validator.verify_type(str)
+                ],
+            },
+            {
+                "field": "is_shown_by",
+                "validations": [
+                    PsplValidator.str_match_ignore_url_protocol,
+                    Validator.verify_type(str)
+                ]
+            },
+        )
+
+    @staticmethod
+    def str_match_ignore_url_protocol(validation_def: dict,
+                                    rikolti_value: Any,
+                                    comparison_value: Any) -> None:
+        if rikolti_value == comparison_value:
+            return
+
+        if comparison_value and comparison_value.startswith('http'):
+            comparison_value = comparison_value.replace('http', 'https')
+
+        if not rikolti_value == comparison_value:
+            return "Content mismatch"
+
+
 class PsplVernacular(OaiVernacular):
     record_cls = PsplRecord
+    validator = PsplValidator
