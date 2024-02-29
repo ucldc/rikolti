d7235121f78232e582e807687e7cf638a9a070ee
amy wieliczka <amywieliczka@gmail.com>
2024-02-29 10:11:48 -0800

    Add on_success_callback and on_failure_callback to content harvest operators

--- dags/shared_tasks/content_harvest_tasks.py
+++ dags/shared_tasks/content_harvest_tasks.py
@@ -3,16 +3,26 @@ from typing import Optional
 
 from airflow.decorators import task, task_group
 
+from rikolti.dags.shared_tasks.shared import notify_rikolti_failure
+from rikolti.dags.shared_tasks.shared import send_log_to_sqs
 from rikolti.utils.versions import get_version
 from rikolti.utils.versions import create_with_content_urls_version
 from rikolti.dags.shared_tasks.content_harvest_operators import ContentHarvestOperator
 
-@task(task_id="create_with_content_urls_version")
+@task(task_id="create_with_content_urls_version", 
+      on_failure_callback=notify_rikolti_failure)
 def create_with_content_urls_version_task(
-    collection: dict, mapped_page_batches: list[str]):
+    collection: dict, mapped_page_batches: list[str], **context):
     mapped_page_batch = json.loads(mapped_page_batches[0])
     mapped_version = get_version(collection['id'], mapped_page_batch[0])
-    return create_with_content_urls_version(mapped_version)
+    with_content_urls_version = create_with_content_urls_version(mapped_version)
+    send_log_to_sqs(
+        context, {'with_content_urls_version': with_content_urls_version})
+    return with_content_urls_version
+
+
+def notify_content_harvest_success(context):
+    send_log_to_sqs(context, {'content_harvest_success': True})
 
 
 @task_group(group_id='content_harvesting')
@@ -29,7 +39,9 @@ def content_harvesting_tasks(
                 task_id="content_harvest", 
                 collection_id="{{ params.collection_id }}",
                 with_content_urls_version=with_content_urls_version,
-                mapper_type=collection['rikolti_mapper_type']
+                mapper_type=collection['rikolti_mapper_type'],
+                on_failure_callback=notify_rikolti_failure,
+                on_success_callback=notify_content_harvest_success
             )
             .expand(
                 pages=mapped_page_batches
