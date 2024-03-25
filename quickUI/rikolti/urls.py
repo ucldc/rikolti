3542e6fe03b3b2f6680e520e329db08d4e8bc2b0
amy wieliczka <amywieliczka@gmail.com>
2024-03-25 13:26:14 -0700

    Remove quick UI django app

--- quickUI/rikolti/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^item/(?P<item_id>.*)/$', views.item_view, name='itemView'),
-    url(r'^$', views.index, name='index'),
-]
