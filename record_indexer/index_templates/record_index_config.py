55f697935551b8c21084ff0682cda58e4a5afb82
amy wieliczka <amywieliczka@gmail.com>
2024-06-10 15:57:24 -0700

    Use english language analysis with asciifolding on all type:text opensearch fields

--- record_indexer/index_templates/record_index_config.py
+++ record_indexer/index_templates/record_index_config.py
@@ -9,42 +9,73 @@ RECORD_INDEX_CONFIG = {
                     "lowercase_trim": {
                         "filter": ["trim", "lowercase"]
                     }
-                }
+                },
+                "filter": {
+                    "english_stop": {
+                        "type": "stop",
+                        "stopwords": "_english_"
+                    },
+                    "english_keywords": {
+                        "type": "keyword_marker",
+                        "keywords": ["example"]
+                    },
+                    "english_stemmer": {
+                        "type": "stemmer",
+                        "language": "english"
+                    },
+                    "english_possessive_stemmer": {
+                        "type": "stemmer",
+                        "language": "possessive_english"
+                    }
+                },
+                "analyzer": {
+                    "asciifolded_english": {
+                        "tokenizer": "standard",
+                        "filter": [
+                            "english_possessive_stemmer",
+                            "lowercase",
+                            "asciifolding",
+                            "english_stop",
+                            "english_keywords",
+                            "english_stemmer"
+                        ]
+                    }
+                },
             }
         },
         "mappings": {
             "dynamic": "strict",
             "properties": {
-                "title": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "alternative_title": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "contributor": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "coverage": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "creator": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "date": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "extent": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "format": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "genre": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "identifier": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "language": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "location": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "publisher": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "relation": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "rights": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "rights_holder": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "rights_note": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "rights_date": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "source": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "spatial": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "subject": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "temporal": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
-                "type": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
+                "title": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "alternative_title": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "contributor": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "coverage": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "creator": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "date": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "extent": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "format": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "genre": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "identifier": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "language": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "location": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "publisher": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "relation": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "rights": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "rights_holder": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "rights_note": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "rights_date": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "source": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "spatial": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "subject": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "temporal": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
+                "type": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
 
                 "sort_title": {"type": "keyword", "normalizer": "lowercase_trim"},
-                "facet_decade": {"type": "text", "fields": {"raw": {"type": "keyword"}}},
+                "facet_decade": {"type": "text", "analyzer": "asciifolded_english", "fields": {"raw": {"type": "keyword"}}},
 
-                "description": {"type": "text"},
-                "provenance": {"type": "text"},
-                "transcription": {"type": "text"},
+                "description": {"type": "text", "analyzer": "asciifolded_english"},
+                "provenance": {"type": "text", "analyzer": "asciifolded_english"},
+                "transcription": {"type": "text", "analyzer": "asciifolded_english"},
 
                 "calisphere-id": {"type": "keyword"},
                 "id": {"type": "keyword"},
