62d63488181fcd58622eed380d7c99b9d63ab1fc
amy wieliczka <amywieliczka@gmail.com>
2024-06-20 16:10:50 -0700

    Remove create_stage_index_task and supporting functions

--- dags/shared_tasks/indexing_tasks.py
+++ dags/shared_tasks/indexing_tasks.py
@@ -5,46 +5,10 @@ from airflow.decorators import task
 
 from rikolti.dags.shared_tasks.shared import notify_rikolti_failure
 from rikolti.dags.shared_tasks.shared import send_event_to_sns
-from rikolti.record_indexer.create_collection_index import create_index_name
-from rikolti.record_indexer.create_collection_index import create_new_index
-from rikolti.record_indexer.create_collection_index import delete_index
 from rikolti.record_indexer.move_index_to_prod import move_index_to_prod
 from rikolti.record_indexer.update_stage_index import update_stage_index_for_collection
 from rikolti.utils.versions import get_version
 
-@task(task_id="create_stage_index", on_failure_callback=notify_rikolti_failure)
-def create_stage_index_task(
-    collection: dict, version_pages: list[str], **context):
-    collection_id = collection.get('id')
-    if not collection_id:
-        raise ValueError(
-            f"Collection ID not found in collection metadata: {collection}")
-
-    index_name = create_index_name(collection_id)
-    try:
-        create_new_index(collection_id, version_pages, index_name)
-    except Exception as e:
-        delete_index(index_name)
-        raise e
-
-    version = get_version(collection_id, version_pages[0])
-    dashboard_query = {"query": {
-        "bool": {"filter": {"terms": {"collection_url": [collection_id]}}}
-    }}
-    print(
-        f"\n\nReview indexed records at: https://rikolti-data.s3.us-west-2."
-        f"amazonaws.com/index.html#{version.rstrip('/')}/data/ \n\n"
-        f"Or on opensearch at: {os.environ.get('RIKOLTI_ES_ENDPOINT')}"
-        "/_dashboards/app/dev_tools#/console with query:\n"
-        f"{json.dumps(dashboard_query, indent=2)}\n\n\n"
-    )
-
-    send_event_to_sns(context, {'index_name': index_name})
-    return index_name
-
-# this task has the same task_id as create_stage_index_task() so that we don't
-# break the Airflow history with the transition between using that task
-# and this one as the last step in the harvest_dag
 @task(task_id="create_stage_index", on_failure_callback=notify_rikolti_failure)
 def update_stage_index_for_collection_task(
     collection: dict, version_pages: list[str], **context):
