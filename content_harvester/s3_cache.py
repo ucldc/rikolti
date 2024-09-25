97efb0ac68ccad1cac0579551279bb4bbe2f40ad
amy wieliczka <amywieliczka@gmail.com>
2024-09-25 11:44:17 -0700

    S3Cache - Add prefix feature

--- content_harvester/s3_cache.py
+++ content_harvester/s3_cache.py
@@ -1,11 +1,19 @@
 import boto3
+from typing import Optional
 
 class S3Cache(object):
     """
     Create a dictionary-like interface to an s3 bucket
+
+    Parameters:
+        bucket_name (str - required): The name of the s3 bucket
+        prefix (str - optional): A string to prepend to all keys
+            include any necessary slashed when specifying the prefix
+        **kwargs: Passed directly to the boto3 client
     """
-    def __init__(self, bucket_name, **kwargs):
+    def __init__(self, bucket_name: str, prefix: Optional[str]=None, **kwargs):
         self.bucket_name = bucket_name
+        self.prefix = prefix or ''
         try:
             self.s3 = boto3.client('s3', **kwargs)
             self.s3.head_bucket(Bucket=self.bucket_name)
@@ -16,10 +24,11 @@ class S3Cache(object):
     def __getitem__(self, key):
         """
         Get the value for the key using bracket notation: S3Cache[key]
-        If the key does not exist, raise the exception with added key context
+        Raise all exceptions with added key context
         """
         try:
-            response = self.s3.get_object(Bucket=self.bucket_name, Key=key)
+            response = self.s3.get_object(
+                Bucket=self.bucket_name, Key=self.prefix+key)
             return response['Body'].read()
         except Exception as e:
             print(f"{self} error getting {key}")
@@ -27,13 +36,17 @@ class S3Cache(object):
 
     def get(self, key, default=None):
         """
-        If the key exists, return the value
-        If the key does not exist (defined by a NoSuchKey exception), 
-        return the default
-        All other exceptions should be raised with added key context
+        Get the value for the key or, if S3 raises NoSuchKey, return the
+        default; raise all other exceptions with added key context
+
+        Parameters:
+            key (str - required): The key to retrieve from the cache
+            default (any - optional): The value to return if the key
+            does not exist [default: None]
         """
         try:
-            response = self.s3.get_object(Bucket=self.bucket_name, Key=key)
+            response = self.s3.get_object(
+                Bucket=self.bucket_name, Key=self.prefix + key)
             return response['Body'].read()
         except self.s3.exceptions.NoSuchKey:
             return default
@@ -44,20 +57,21 @@ class S3Cache(object):
     def __setitem__(self, key, data):
         """
         Set the key to the data via assignment: S3Cache[key] = data
-        Raise any exceptions with added key context
+        Raise all exceptions with added key context
         """
         try:
-            self.s3.put_object(Bucket=self.bucket_name, Key=key, Body=data)
+            self.s3.put_object(
+                Bucket=self.bucket_name, Key=self.prefix + key, Body=data)
         except Exception as e:
             print(f"{self} error setting {key}: {e}")
             raise e
 
     def __contains__(self, key):
         """
-        Check if the key exists:
-            True is a successful head request
+        Check if the key exists in the cache using the 'in' operator:
+            True is a successful S3 head request
             False is a 404 ClientError
-        All other exceptions should be raised with added key context
+        Raise all other exceptions with added key context
         """
         try:
             self.s3.head_object(Bucket=self.bucket_name, Key=key)
@@ -76,4 +90,7 @@ class S3Cache(object):
         self.s3.delete_object(Bucket=self.bucket_name, Key=key)
 
     def __repr__(self):
-        return f"S3Cache(bucket_name={self.bucket_name})"
+        readable = f"S3Cache(bucket_name={self.bucket_name}"
+        if self.prefix:
+            return f"{readable}, prefix={self.prefix})"
+        return f"{readable})"
