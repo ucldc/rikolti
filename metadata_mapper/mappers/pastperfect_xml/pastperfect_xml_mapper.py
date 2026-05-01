915aacb534eff7c69c23477f9350322ec24d4cca
Barbara Hui <barbara.hui@ucop.edu>
2026-05-01 14:33:22 -0700

    Use updated ensure_list mapper helper function

--- metadata_mapper/mappers/pastperfect_xml/pastperfect_xml_mapper.py
+++ metadata_mapper/mappers/pastperfect_xml/pastperfect_xml_mapper.py
@@ -9,18 +9,18 @@ class PastperfectXmlRecord(Record):
             "calisphere-id": self.legacy_couch_db_id.split('--')[1],
             "isShownAt": self.map_is_shown_at,
             "isShownBy": self.map_is_shown_by,
-            "title": self.ensure_list("title"),
+            "title": self.ensure_list(self.source_metadata.get("title")),
             "date": self.map_date,
-            "description": self.ensure_list("description"),
+            "description": self.ensure_list(self.source_metadata.get("description")),
             "subject": self.map_subject,
-            "spatial": self.ensure_list("spatial"),
+            "spatial": self.ensure_list(self.source_metadata.get("spatial")),
             "temporal": self.source_metadata.get("coverage"),
             "format": self.collate_fields(["medium", "material"]),
             "creator": self.collate_fields(["creator", "author", "artist",
                                             "photographer"]),
             "identifier": self.collate_fields(["identifier", "objectid", "arkid"]),
             "type": self.source_metadata.get("objectname"),
-            "relation": self.ensure_list("collection"),
+            "relation": self.ensure_list(self.source_metadata.get("collection")),
             "rights": self.source_metadata.get("rights")
         }
 
