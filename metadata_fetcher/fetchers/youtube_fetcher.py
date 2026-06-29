4bd8b3f0067942ba2a588a7244671dab06269272
Barbara Hui <barbara.hui@ucop.edu>
2026-06-29 16:39:09 -0700

    Support harvest url for youtube API videos resource with multiple ids supplied

--- metadata_fetcher/fetchers/youtube_fetcher.py
+++ metadata_fetcher/fetchers/youtube_fetcher.py
@@ -6,6 +6,11 @@ from urllib.parse import urlparse
 from .. import settings
 from .Fetcher import Fetcher, FetchError
 
+def is_playlist(url):
+    if urlparse(url).path == '/youtube/v3/playlistItems':
+        return True
+    return False
+
 class YoutubeFetcher(Fetcher):
 
     def __init__(self, params):
@@ -21,37 +26,10 @@ class YoutubeFetcher(Fetcher):
         - https://www.googleapis.com/youtube/v3/playlistItems?playlistId={id}
         - https://www.googleapis.com/youtube/v3/videos?id={id}
         """
-        if urlparse(self.url).path == '/youtube/v3/playlistItems':
-            # The `playlistItems` API endpoint returns incomplete metadata, so we hit it
-            # for a page of video ids, and then hit the `videos` endpoint for full metadata.
-            playlist_content_url = (
-                f"{self.url}"
-                f"&key={settings.YOUTUBE_API_KEY}"
-                f"&part=contentDetails"
-                f"&maxResults=50"
-            )
-
-            if self.next_page_token:
-                playlist_content_url += f"&pageToken={self.next_page_token}"
-
-            playlist_content_request = {"url": playlist_content_url}
-            try:
-                response = self.http_session.get(**playlist_content_request)
-                response.raise_for_status()
-            except requests.exceptions.HTTPError as e:
-                raise FetchError(
-                    f"[{self.collection_id}]: unable to fetch {playlist_content_request}",
-                    f"Error was: {e}"
-                )
-
-            json_response = response.json()
-
-            self.next_page_token = json_response.get("nextPageToken")
-
-            video_ids = [item.get("contentDetails").get("videoId") for item in json_response.get("items",[])]
-            videos_url = f"https://www.googleapis.com/youtube/v3/videos?id={','.join(video_ids)}"
+        if is_playlist(self.url):
+            videos_url = self.iterate_playlist()
         else:
-            videos_url = self.url
+            videos_url = self.url.strip("/")
 
         url = (
             f"{videos_url}"
@@ -60,6 +38,9 @@ class YoutubeFetcher(Fetcher):
             f"&maxResults=50"
         )
 
+        if not is_playlist(self.url) and self.next_page_token:
+            url += f"&pageToken={self.next_page_token}"
+
         request = {"url": url}
 
         print(
@@ -67,6 +48,38 @@ class YoutubeFetcher(Fetcher):
             f"at {request.get('url')}")
 
         return request
+
+    def iterate_playlist(self):
+        # The `playlistItems` API endpoint returns incomplete metadata, so we hit it
+        # for a page of video ids, and then hit the `videos` endpoint for full metadata.
+        playlist_content_url = (
+            f"{self.url}"
+            f"&key={settings.YOUTUBE_API_KEY}"
+            f"&part=contentDetails"
+            f"&maxResults=50"
+        )
+
+        if self.next_page_token:
+            playlist_content_url += f"&pageToken={self.next_page_token}"
+
+        playlist_content_request = {"url": playlist_content_url}
+        try:
+            response = self.http_session.get(**playlist_content_request)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            raise FetchError(
+                f"[{self.collection_id}]: unable to fetch {playlist_content_request}",
+                f"Error was: {e}"
+            )
+
+        json_response = response.json()
+
+        self.next_page_token = json_response.get("nextPageToken")
+
+        video_ids = [item.get("contentDetails").get("videoId") for item in json_response.get("items",[])]
+        videos_url = f"https://www.googleapis.com/youtube/v3/videos?id={','.join(video_ids)}"
+
+        return videos_url
 
     def check_page(self, http_resp) -> int:
         data = json.loads(http_resp.content)
@@ -79,6 +92,13 @@ class YoutubeFetcher(Fetcher):
             )
 
         return len(items)
+    
+    def increment(self, http_resp):
+        self.write_page = self.write_page + 1
+        
+        if not is_playlist(self.url):
+            data = http_resp.json()
+            self.next_page_token = data.get("nextPageToken")
 
     def json(self) -> str:
         if not self.next_page_token:
