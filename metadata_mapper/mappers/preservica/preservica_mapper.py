021a42e842377709477f3ec820ecaf074c90c1eb
Barbara Hui <barbara.hui@ucop.edu>
2026-07-14 11:27:51 -0700

    Update mapper to work with object detail vernacular

--- metadata_mapper/mappers/preservica/preservica_mapper.py
+++ metadata_mapper/mappers/preservica/preservica_mapper.py
@@ -4,7 +4,7 @@ from ..mapper import Vernacular, Record
 
 class PreservicaRecord(Record):
     def to_UCLDC(self):
-        id_handle = self.source_metadata.get('id')
+        id_handle = self.remove_id_prefix(self.source_metadata.get('id'))
         self.legacy_couch_db_id = f"{self.collection_id}--{id_handle}"
         return super().to_UCLDC()
 
@@ -13,52 +13,64 @@ class PreservicaRecord(Record):
             "calisphere-id": self.legacy_couch_db_id.split('--')[1],
             "isShownAt": self.map_is_shown_at,
             "isShownBy": self.map_is_shown_by,
-            "contributor": self.ensure_list(self.source_metadata.get("oai_dc.contributor")),
-            "spatial": self.ensure_list(self.source_metadata.get("oai_dc.coverage")),
-            "creator": self.ensure_list(self.source_metadata.get("oai_dc.creator")),
-            "date": self.source_metadata.get("oai_dc.date"),
-            "description": self.ensure_list(self.source_metadata.get("oai_dc.description")),
-            "format": self.ensure_list(self.source_metadata.get("oai_dc.format")),
-            "identifier": self.ensure_list(self.source_metadata.get("oai_dc.identifier")),
-            "language": self.ensure_list(self.source_metadata.get("oai_dc.language")),
-            "publisher": self.ensure_list(self.source_metadata.get("oai_dc.publisher")),
-            "relation": self.ensure_list(self.source_metadata.get("oai_dc.relation")),
-            "rights": self.ensure_list(self.source_metadata.get("oai_dc.rights")),
-            "source": self.ensure_list(self.source_metadata.get("oai_dc.source")),
-            # "subject": self.ensure_list(self.source_metadata.get("oai_dc.subject")),
+            "contributor": self.source_metadata.get("contributor"),
+            "spatial": self.source_metadata.get("coverage"),
+            "creator": self.source_metadata.get("creator"),
+            "date": self.source_metadata.get("date"),
+            "description": self.source_metadata.get("description"),
+            "format": self.source_metadata.get("format"),
+            "identifier": self.source_metadata.get("identifier"),
+            "language": self.source_metadata.get("language"),
+            "publisher": self.source_metadata.get("publisher"),
+            "relation": self.source_metadata.get("relation"),
+            "rights": self.source_metadata.get("rights"),
+            "source": self.source_metadata.get("source"),
             "subject": self.map_subject,
-            "title": self.ensure_list(self.source_metadata.get("oai_dc.title")),
-            "type": self.ensure_list(self.source_metadata.get("oai_dc.type")),
+            "title": self.source_metadata.get("title"),
+            "type": self.source_metadata.get("type"),
         }
     
-    def map_is_shown_at(self) -> str:
-        id = self.source_metadata.get('id')
+    def remove_id_prefix(self, id) -> str:
+        return id.removeprefix('sdb:IO|')
 
+    def map_is_shown_at(self) -> str:
+        id = self.remove_id_prefix(self.source_metadata.get('id'))
         return f"https://oakland.access.preservica.com/file/sdb:digitalFile%7C{id}/"
     
     def map_is_shown_by(self) -> str:
-        id = self.source_metadata.get('id')
+        id = self.remove_id_prefix(self.source_metadata.get('id'))
         return f"https://oakland.access.preservica.com/download/thumbnail/sdb:digitalFile%7C{id}"
 
     def map_subject(self) -> list:
-        subjects = self.source_metadata.get("oai_dc.subject")
+        subjects = self.source_metadata.get("subject")
         if subjects:
-            subjects = self.ensure_list(subjects)
-            subject = [{"name": subject} for subject in subjects]
-
-            return subject
+            return [{"name": subject} for subject in subjects]
 
 class PreservicaVernacular(Vernacular):
     record_cls = PreservicaRecord
 
     def parse(self, api_response):
         data = json.loads(api_response)
-        vernacular_metadata = data.get("value",{}).get("metadata", [])
         records = []
-        for vm in vernacular_metadata:
+        for item in data:
             record = {}
-            for field in vm:
-                record[field['name']] = field['value']
+            record["id"] = item.get("id")
+
+            dc_metadata = [
+                    group for group
+                    in item.get("metadata", {}).get("groupOrItem", [])
+                    if group.get("title") == "Dublin Core Metadata"
+                ]
+
+            for field in dc_metadata[0]["groupOrItem"]:
+                field_name = field["name"]
+                field_value = field["value"]
+                if field_value:
+                    if field_name in record:
+                        record[field_name].append(field_value)
+                    else:
+                        record[field_name] = [field_value]
+
             records.append(record)
 
         return self.get_records(records)
