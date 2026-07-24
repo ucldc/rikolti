f90136b2fa5ef6af222a8e6abc36050a9c34f124
Barbara Hui <barbara.hui@ucop.edu>
2026-07-24 11:40:05 -0700

    Add oai.omeka.chssc mapper (#1343)

---
+++ metadata_mapper/mappers/oai/omeka/chssc_mapper.py
@@ -0,0 +1,18 @@
+import re
+from ..omeka_mapper import OmekaRecord, OmekaVernacular
+
+class ChsscRecord(OmekaRecord):
+    def UCLDC_map(self):
+        return {
+            "isShownAt": self.map_is_shown_at
+        }
+
+    def map_is_shown_at(self):
+        for identifier in self.source_metadata.get('identifier'):
+            identifier = identifier.strip()
+            if re.search(r"^http*s:\/\/", identifier) \
+            and re.search(r"/item/[0-9]+/?$", identifier):
+                return identifier
+
+class ChsscVernacular(OmekaVernacular):
+    record_cls = ChsscRecord
